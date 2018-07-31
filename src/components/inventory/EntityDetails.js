67890ac236c7ad81913a6b547849d15e879a8df6
Jozef Hartinger <jharting@redhat.com>
2018-07-31 08:41:16 +0200

    present more details for an entity

--- src/components/inventory/EntityDetails.js
+++ src/components/inventory/EntityDetails.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Title } from '@patternfly/react-core';
 import { SyncAltIcon } from '@patternfly/react-icons';
+import get from 'lodash/get';
+
 import Breadcrumbs from '../Breadcrumbs';
 
 import './EntityDetails.scss';
-
-const TYPE_PLACEHOLDER = 'RHEL Box';
 
 // MARK: maybe implement this in components??
 const EntityDetails = ({ loaded, entity }) => {
@@ -18,19 +18,23 @@ const EntityDetails = ({ loaded, entity }) => {
         );
     }
 
+    const getFact = path => get(entity, path, 'unknown');
+
     return (
         <React.Fragment>
             <Breadcrumbs name='Inventory' path='/inventory' param={entity.display_name}/>
             <Title size='2xl'>{entity.display_name}</Title>
             <dl>
-                <dt>hostname</dt>
-                <dd>{entity.display_name}</dd>
-                <dt>system id</dt>
+                <dt>Hostname</dt>
+                <dd>{getFact('display_name')}</dd>
+                <dt>System ID</dt>
                 <dd>{entity.id}</dd>
-                <dt>type</dt>
-                <dd>{TYPE_PLACEHOLDER}</dd>
-                <dt>account</dt>
-                <dd>{entity.account}</dd>
+                <dt>Canonical System ID</dt>
+                <dd>{getFact(`canonical_facts['machine-id']`)}</dd>
+                <dt>System</dt>
+                <dd>{getFact('facts.release')}</dd>
+                <dt>Timezone</dt>
+                <dd>{getFact('facts.timezone_information.timezone')}</dd>
             </dl>
         </React.Fragment>
     );
