e6f9e6ce5dd510a75b159506ec1cf4d6dfd34ace
Jozef Hartinger <jharting@redhat.com>
2018-08-17 16:42:36 +0200

    fall back to mock data when API is not available

--- src/api.js
+++ src/api.js
@@ -1,9 +1,54 @@
 import { INVENTORY_API_BASE } from './config';
+import { getStore } from './store';
+import { addAlert } from './actions';
+
+
+function buildMock (i) {
+    return {
+        "id": i,
+        "canonical_facts": {
+            "machine-id": `${i}c1497de-0ec7-43bb-a8a6-35cabd59e0bf`
+        },
+        "account": "000001",
+        "facts": {
+            "release": "Red Hat Enterprise Linux Server release 7.5 (Maipo)",
+            "rhel_version": "7.5",
+            "host_system_id": "6c1497de-0ec7-43bb-a8a6-35cabd59e0bf",
+            "bios_information": {
+                "vendor": "SeaBIOS",
+                "version": "1.10.2-3.el7_4.1",
+                "release_date": "04/01/2014",
+                "bios_revision": "0.0"
+            },
+            "system_information": {
+                "family": "Red Hat Enterprise Linux",
+                "manufacturer": "Red Hat",
+                "product_name": "RHEV Hypervisor",
+                "virtual_machine": true
+            },
+            "listening_processes": [],
+            "timezone_information": {
+                "timezone": "EDT",
+                "utcoffset": -14400
+            }
+        },
+        "display_name": `server0${i}.redhat.com`
+    };
+};
+
+const MOCKS = Array.from({length: 5}).map((v, i) => buildMock(i));
 
 export function getEntities () {
     return fetch(INVENTORY_API_BASE).then(r => {
         if (r.ok) {
             return r.json();
+        }
+
+        if (r.status === 404) {
+            getStore().dispatch(addAlert({
+                title: 'Unable to connect to the API. Falling back to mock data set'
+            }));
+            return MOCKS;
         }
 
         throw new Error(`Unexpected response code ${r.status}`);
