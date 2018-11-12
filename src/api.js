d6b1cad0b47db96d0b542b4878962e5c7036160b
Karel Hala <khala@redhat.com>
2018-11-12 12:45:58 +0100

    Connect inventory to real API

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import { INVENTORY_API_BASE } from './config';
 /* eslint camelcase: off */
 function buildMock (i) {
     return {
-        id: i,
+        id: `${i}`,
         canonical_facts: {
             machine_id: `${i}c1497de-0ec7-43bb-a8a6-35cabd59e0bf`
         },
@@ -44,33 +44,43 @@ function buildMock (i) {
     };
 }
 
-const MOCKS = Array.from({ length: 5 }).map((v, i) => buildMock(i));
+const mockData = (id) => {
+    const MOCKS = Array.from({ length: 5 }).map((v, i) => buildMock(i));
+    return {
+        results: id ? MOCKS.find(oneResult => id === oneResult.id) : MOCKS,
+        isMocked: true
+    };
+};
+
 let mockWarnCount = 0;
+
+const addMockApp = (results) => results.map(row => ({
+    id: row.id,
+    health: {
+        vulnerabilities: { title: 5, redirect: 'cost_management' },
+        configuration: { title: 10, redirect: 'configuration_assessment' },
+        compliance: { title: '74%', redirect: 'compliance' },
+        cost: { title: '23K', redirect: 'cost_management' }
+    }
+}));
 
 export function getEntities () {
     return fetch(INVENTORY_API_BASE).then(r => {
         if (r.ok) {
-            return r.json().then(data => data.map(row => ({
-                id: row.id,
-                health: {
-                    vulnerabilities: { title: 5, redirect: 'cost_management' },
-                    configuration: { title: 10, redirect: 'configuration_assessment' },
-                    compliance: { title: '74%', redirect: 'compliance' },
-                    cost: { title: '23K', redirect: 'cost_management' }
-                }
-            })));
+            return r.json().then(data => ({
+                results: addMockApp(data.results)
+            }));
         }
 
         if (r.status === 404) {
             if (!mockWarnCount) {
                 mockWarnCount++;
-
+                return new Promise((resolve) => resolve(mockData())).then(data => {
+                    return ({
+                        results: addMockApp(data.results)
+                    });
+                });
             }
-
-            return {
-                items: MOCKS,
-                isMocked: true
-            };
         }
 
         throw new Error(`Unexpected response code ${r.status}`);
@@ -78,7 +88,16 @@ export function getEntities () {
 }
 
 export function getEntity (id) {
-    return getEntities().then(entities => {
-        return (entities.items || entities).find(e => e.id === id);
+    return fetch(`${INVENTORY_API_BASE}/${id}`).then(r => {
+        if (r.ok) {
+            return r.json();
+        }
+
+        // TODO: remove me
+        if (r.status === 404) {
+            return mockData(id);
+        }
+
+        throw new Error(`Unexpected response code ${r.status}`);
     });
 }
