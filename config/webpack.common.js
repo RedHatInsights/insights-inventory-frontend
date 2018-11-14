55224b9293a7fb2c29955d8d4200013adcacbf2f
Josh <jkinlaw@redhat.com>
2018-11-14 08:09:40 -0500

    adds beta support

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production' && betaBranch) {
     insightsDeployment = 'insightsbeta';
 }
 
-const publicPath = `/${insightsDeployment}/platform/advisor/`;
+const publicPath = `/${insightsDeployment}/platform/inventory/`;
 
 module.exports = {
     paths: {
