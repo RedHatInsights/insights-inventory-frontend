676f4dd7d29b4986fa254afcc8b3316489e89236
Josh <jkinlaw@redhat.com>
2018-11-14 08:09:40 -0500

    adds beta support

--- config/base.webpack.plugins.js
+++ config/base.webpack.plugins.js
@@ -7,6 +7,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const plugins = [];
+const config = require('./webpack.common.js');
 
 /**
  * Writes bundles to distribution folder.
@@ -86,5 +87,23 @@ const CopyFilesWebpackPlugin = new (require('copy-webpack-plugin'))([
     { from: path.resolve(__dirname, '../static/images'), to: 'images' }
 ]);
 plugins.push(CopyFilesWebpackPlugin);
+
+/**
+ * Replaces any @@insights in the html files with config.insightsDeployment value.
+ * This handles the path being either insights or insightsbeta in the esi:include.
+ */
+const HtmlReplaceWebpackPlugin = new(require('html-replace-webpack-plugin'))([{
+    pattern: '@@insights',
+    replacement: config.insightsDeployment
+}]);
+plugins.push(HtmlReplaceWebpackPlugin);
+
+/**
+ * Replaces any instance of RELEASE in js files with config.insightsDeployment value.
+ */
+const Release = new webpack.DefinePlugin({
+    RELEASE: JSON.stringify(config.insightsDeployment)
+});
+plugins.push(Release);
 
 module.exports = { plugins };
