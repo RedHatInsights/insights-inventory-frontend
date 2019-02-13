f8b87a7518f982439341d7dad74ad41d77f4333e
Daniel Lobato García <me@daniellobato.me>
2019-02-13 10:36:11 +0100

    Remove inventory CI URL (#50)
    It's probably not a good idea to keep the CI inventory URL out in the open, so that it may not be abused

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -1,5 +1,5 @@
 /* global exports */
 
 exports.routes = {
-    '/hosts': { host: 'https://insights-inventory-platform-ci.5a9f.insights-dev.openshiftapps.com/api' }
+    '/hosts': { host: 'https://insights-inventory-platform-ci/api' }
 };
