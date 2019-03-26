522083d2db5b8602995696f3b27af25af1b7d334
Karel Hala <khala@redhat.com>
2019-03-26 09:00:41 +0100

    Remove qa and ci from custom push

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -10,7 +10,6 @@ elif [[ "${TRAVIS_BRANCH}" = "master-stable" ]]; then
     .travis/release.sh "ci-stable"
     .travis/release.sh "qa-stable"
 
-# If current dev branch is deployment branch, push to build repo
-elif [[ "${TRAVIS_BRANCH}" = "ci-stable"  || "${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+elif [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     .travis/release.sh "${TRAVIS_BRANCH}"
 fi
