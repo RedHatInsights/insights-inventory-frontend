cedc49bc6733a4e317a6aefa73a322541bd0752e
Karel Hala <ch4rlien@gmail.com>
2019-04-18 15:52:06 +0200

    Update custom release (#86)

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -3,12 +3,24 @@ set -e
 set -x
 
 if [[ "${TRAVIS_BRANCH}" = "master" ]]; then
-    .travis/release.sh "ci-beta"
-    .travis/release.sh "qa-beta"
+    for env in ci qa
+    do
+        echo
+        echo
+        echo "PUSHING ${env}-beta"
+        rm -rf ./build/.git
+        .travis/release.sh "${env}-beta"
+    done
     
 elif [[ "${TRAVIS_BRANCH}" = "master-stable" ]]; then
-    .travis/release.sh "ci-stable"
-    .travis/release.sh "qa-stable"
+    for env in ci qa
+    do
+        echo
+        echo
+        echo "PUSHING ${env}-stable"
+        rm -rf ./build/.git
+        .travis/release.sh "${env}-stable"
+    done
 
 elif [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     .travis/release.sh "${TRAVIS_BRANCH}"
