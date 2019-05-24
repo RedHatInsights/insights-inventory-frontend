2de2eec82dd5ce4134438fd3aa0b819ea89d7d1c
Karel Hala <khala@redhat.com>
2019-05-24 16:15:28 +0200

    Clean dependencies and use new package to build repo

--- src/entry-dev.js
+++ src/entry-dev.js
@@ -5,19 +5,11 @@ import { Provider } from 'react-redux';
 import { init } from './store';
 import App from './App';
 import logger from 'redux-logger';
-
-const pathName = window.location.pathname.split('/');
-pathName.shift();
-
-let release = '/';
-
-if (pathName[0] === 'beta') {
-    release = `/${pathName.shift()}/`;
-}
+import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/files/helpers';
 
 ReactDOM.render(
     <Provider store={init(logger).getStore()}>
-        <Router basename={`${release}${pathName[0]}/${pathName[1]}`}>
+        <Router basename={getBaseName(window.location.pathname)}>
             <App />
         </Router>
     </Provider>,
