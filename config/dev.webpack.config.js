0dbdb0e794a7894e1a91406c55d5dd84f8667415
Karel Hala <khala@redhat.com>
2019-06-07 15:12:46 +0200

    Use https when serving content

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -3,6 +3,7 @@ const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../'),
+    https: true,
     debug: true
 });
 
