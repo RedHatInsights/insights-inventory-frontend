05a1dec5c3f549256fb3dfbdd79744424727c7be
Karel Hala <khala@redhat.com>
2019-07-02 17:48:14 +0200

    Add delete operation to inventory table (#103)
    * Add delete operation to inventory table
    
    * Change toast messages when remove initiated and finished

--- src/api.js
+++ src/api.js
@@ -1,5 +1,10 @@
 import { INVENTORY_API_BASE } from './config';
 
+import instance from '@redhat-cloud-services/frontend-components-utilities/files/interceptors';
+import { HostsApi } from '@redhat-cloud-services/host-inventory-client';
+
+export const hosts = new HostsApi(undefined, INVENTORY_API_BASE, instance);
+
 export async function getAllEntities({ filters = [] }) {
     const config = {
         // eslint-disable-next-line camelcase
