f9b402850f5ef63aa2da032fb7a27f1b24ab8dae
Karel Hala <khala@redhat.com>
2019-10-31 16:27:54 +0100

    Address codereview changes with new Map

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -52,14 +52,14 @@ function entitySelected(state, { payload }) {
     const selected = state.selected || (new Map());
     if (payload.selected) {
         if (payload.id === 0) {
-            state.rows.map(row => selected.set(row.id, row));
+            state.rows.forEach(row => selected.set(row.id, row));
         } else {
             const selectedRow = state.rows && state.rows.find(({ id }) => id === payload.id);
             selected.set(payload.id, { ...selectedRow || {}, id: payload.id });
         }
     } else {
         if (payload.id === 0) {
-            state.rows.map(row => selected.delete(row.id));
+            state.rows.forEach(row => selected.delete(row.id));
         } else if (payload.id === -1) {
             selected.clear();
         } else {
@@ -69,7 +69,7 @@ function entitySelected(state, { payload }) {
 
     return {
         ...state,
-        selected
+        selected: new Map(selected)
     };
 }
 
