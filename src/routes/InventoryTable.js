2c6e40d001efe47b49e9ce3338981dedb0c248a4
Karel Hala <khala@redhat.com>
2020-04-07 08:39:34 +0200

    Hide tags

--- src/routes/InventoryTable.js
+++ src/routes/InventoryTable.js
@@ -102,7 +102,7 @@ const Inventory = ({
                                     store={store}
                                     ref={inventory}
                                     hasCheckbox
-                                    showTags
+                                    // showTags
                                     onRefresh={onRefresh}
                                     {
                                         ...loaded && rows && rows.length > 0 && {
