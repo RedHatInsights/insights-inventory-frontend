31db969f20bc938f58b9e15f275b64cf9283a8b6
Karel Hala <khala@redhat.com>
2020-04-14 19:57:34 +0200

    Pass inv ID from router to compliance (#429)

--- src/components/inventory/Compliance.js
+++ src/components/inventory/Compliance.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Compliance from '@redhat-cloud-services/frontend-components-inventory-compliance';
 import '@redhat-cloud-services/frontend-components-inventory-compliance/index.css';
 import { IntlProvider } from 'react-intl';
+import { useParams } from 'react-router-dom';
 
 const ComplianceTab = () => (
     <IntlProvider locale={navigator.language}>
-        <Compliance />
+        <Compliance inventoryId={ useParams().inventoryId }/>
     </IntlProvider>
 );
 
