2a864829916ca03749d3ad794df3df168b52e1e6
Karel Hala <khala@redhat.com>
2020-04-20 17:24:59 +0200

    Update vuln package and use intl provider from it

--- src/routes/InventoryDetail.js
+++ src/routes/InventoryDetail.js
@@ -77,7 +77,7 @@ const Inventory = ({ entity, currentApp, clearNotifications, loadEntity }) => {
                         }
                     </BreadcrumbItem>
                 </Breadcrumb>
-                {InventoryDetail && <InventoryDetail hideBack showTags />}
+                {InventoryDetail && <InventoryDetail hideBack showTags hideInvLink />}
             </PageHeader>
             <Main className={classnames(additionalClasses)}>
                 <Grid gutter="md">
