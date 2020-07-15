aa1ddb05d1469775b1d4bfa9728eff1941307ea7
Karel Hala <khala@redhat.com>
2020-07-15 15:49:22 +0200

    use hot fix version of compliance and use it so UI won't blow up

--- src/components/inventory/Compliance.js
+++ src/components/inventory/Compliance.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Compliance from '@redhat-cloud-services/frontend-components-inventory-compliance';
 import '@redhat-cloud-services/frontend-components-inventory-compliance/index.css';
-import { IntlProvider } from 'react-intl';
 import { useParams } from 'react-router-dom';
 
-const ComplianceTab = () => (
-    <IntlProvider locale={navigator.language}>
-        <Compliance inventoryId={ useParams().inventoryId }/>
-    </IntlProvider>
-);
+const ComplianceTab = () => <Compliance customItnl intlProps={{
+    locale: navigator.language
+}} inventoryId={ useParams().inventoryId } />;
 
 export default ComplianceTab;
