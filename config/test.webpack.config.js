51c9ea0d34e2edcf09020524d33a3618a1b349d7
Karel Hala <khala@redhat.com>
2020-08-07 18:03:03 +0200

    Update packages and include new PF react alias (#694)

--- config/test.webpack.config.js
+++ config/test.webpack.config.js
@@ -5,7 +5,8 @@ const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({ rootFolder: resolve(__dirname, '../') });
 
 webpackConfig.resolve.alias = {
-    customReact: 'react'
+    customReact: 'react',
+    PFReactCore: '@patternfly/react-core'
 };
 
 module.exports = { ...webpackConfig, plugins };
