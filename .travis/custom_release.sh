e11bfc46df6a62cc8841dcfc4364987546f42234
Karel Hala <khala@redhat.com>
2020-09-10 10:22:25 +0200

    Release new version of all packages, update custom release (#766)

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,16 +2,26 @@
 set -e
 set -x
 
-if [[ "${TRAVIS_BRANCH}" = "master" ]]; then
+if [ "${TRAVIS_BRANCH}" = "master" ]
+then
     for env in ci qa
     do
-        echo
-        echo
         echo "PUSHING ${env}-beta"
-        rm -rf ./build/.git
+        rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
     done
 
+elif [ "${TRAVIS_BRANCH}" = "master-stable" ]
+then
+    for env in ci qa
+    do
+        echo "PUSHING ${env}-stable"
+        rm -rf ./dist/.git
+        .travis/release.sh "${env}-stable"
+    done
+
 elif [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "ci-stable" ]]; then
+    echo "PUSHING ${TRAVIS_BRANCH}"
+    rm -rf ./build/.git
     .travis/release.sh "${TRAVIS_BRANCH}"
 fi
