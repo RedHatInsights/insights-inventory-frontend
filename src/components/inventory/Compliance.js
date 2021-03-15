56189a42fae24c80a828aac7f3fbbff9b6584ba9
Katerina Patticha <apattich@redhat.com>
2021-03-15 13:32:52 +0100

    fix(compliance): Fix undefined inventoryId

--- src/components/inventory/Compliance.js
+++ src/components/inventory/Compliance.js
@@ -1,12 +1,18 @@
 import React, { lazy, Suspense } from 'react';
-import { useParams } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 
 const Compliance = lazy(() => import('@redhat-cloud-services/frontend-components-inventory-compliance/Compliance'));
 
-const ComplianceTab = () => <Suspense fallback="">
-    <Compliance customItnl intlProps={{
-        locale: navigator.language
-    }} inventoryId={ useParams().inventoryId } />
-</Suspense>;
+const ComplianceTab = () => {
+    const { params } = useRouteMatch('/:inventoryId');
+
+    return (
+        <Suspense fallback="">
+            <Compliance customItnl intlProps={{
+                locale: navigator.language
+            }} inventoryId={ params.inventoryId } />
+        </Suspense>
+    );
+};
 
 export default ComplianceTab;
