748b80e91d6e05d8a40b0eb93b4880b377a4eaeb
Karel Hala <khala@redhat.com>
2021-05-10 16:39:22 +0200

    Migrate general info tab to federated modules

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,5 +1,6 @@
 /* global module, __dirname */
 const { resolve } = require('path');
+const webpack = require('webpack');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../'),
@@ -16,9 +17,17 @@ const { config: webpackConfig, plugins } = config({
 plugins.push(
     require('@redhat-cloud-services/frontend-components-config/federated-modules')({
         root: resolve(__dirname, '../'),
-        useFileHash: false
+        useFileHash: false,
+        exposes: {
+            './RootApp': resolve(__dirname, '../src/AppEntry'),
+            './SystemDetail': resolve(__dirname, '../src/components/inventory/GeneralInfo')
+        }
     })
 );
+
+plugins.push(new webpack.DefinePlugin({
+    IS_DEV: true
+}));
 
 webpackConfig.resolve.alias = {
     ...webpackConfig.resolve.alias,
