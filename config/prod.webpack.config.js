20c7f41237cf9dfeb5e0a0b2271ce91e675fc790
Ryan Long <rlong@redhat.com>
2021-05-14 12:39:03 -0400

    define webpack

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* global module, __dirname */
 const { resolve } = require('path');
+const webpack = require('webpack');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../'),
