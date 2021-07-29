3ba65e431960976698a0749b3ee503309925ad24
Karel Hala <khala@redhat.com>
2021-07-29 15:08:44 +0200

    Re-add ouia attrs to delete modal

--- src/Utilities/DeleteModal.js
+++ src/Utilities/DeleteModal.js
@@ -28,6 +28,7 @@ const DeleteModal = ({ handleModalToggle, isModalOpen, currentSytems, onConfirm 
         variant="small"
         title="Remove from inventory"
         className="ins-c-inventory__table--remove"
+        ouiaId="inventory-delete-modal"
         isOpen={isModalOpen}
         onClose={() => handleModalToggle(false)}
         appendTo={document.getElementsByClassName('inventory')[0] || document.body}
@@ -55,7 +56,13 @@ const DeleteModal = ({ handleModalToggle, isModalOpen, currentSytems, onConfirm 
                 <Button variant="danger" onClick={onConfirm}>
                     Remove
                 </Button>
-                <Button variant="link" onClick={() => handleModalToggle(false)}>Cancel</Button>
+                <Button
+                    variant="link"
+                    ouiaId="cancel-delete"
+                    onClick={() => handleModalToggle(false)}
+                >
+                    Cancel
+                </Button>
             </LevelItem>
         </Level>
     </Modal>;
