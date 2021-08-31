9588863b85a85746f24109ee0ac1fe92da541f5f
bholifie <bholifie@redhat.com>
2021-08-31 15:10:51 -0500

    test and get feedback on first attempt at build pipeline

---
+++ build_deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -exv
+
+IMAGE="quay.io/cloudservices/insights-inventory-frontend"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+
+if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+    echo "QUAY_USER and QUAY_TOKEN must be set"
+    exit 1
+fi
+
+
+AUTH_CONF_DIR="$(pwd)/.podman"
+mkdir -p $AUTH_CONF_DIR
+export REGISTRY_AUTH_FILE="$AUTH_CONF_DIR/auth.json"
+
+podman login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+podman login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
+podman build --pull=true -f Dockerfile -t "${IMAGE}:${IMAGE_TAG}" .
+podman push "${IMAGE}:${IMAGE_TAG}"
+
+podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
+podman push "${IMAGE}:latest"
---
+++ pr_check.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# --------------------------------------------
+# Export vars for helper scripts to use
+# --------------------------------------------
+export APP_NAME="host-inventory" # name of app-sre "application" folder this component lives in
+export IMAGE="quay.io/cloudservices/insights-inventory-frontend"
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
+export APP_ROOT=$(pwd)
+export WORKSPACE=${WORKSPACE:-$APP_ROOT}  # if running in jenkins, use the build's workspace
+export IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+export GIT_COMMIT=$(git rev-parse HEAD)
+cat /etc/redhat-release
+
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+COMPONENT_NAME="insights-inventory-frontend"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IQE_PLUGINS="host_inventory"
+IQE_MARKER_EXPRESSION="smoke"
+IQE_FILTER_EXPRESSION=""
+
+# ---------------------------
+# Build and Publish to Quay
+# ---------------------------
+
+npm ci
+npm run verify
+# npx codecov
+
+NPM_INFO="undefined"
+PATTERNFLY_DEPS="undefined"
+if [[ -f package-lock.json ]] || [[ -f yarn.lock ]];
+then
+  LINES=`npm list --silent --depth=0 --production | grep @patternfly -i | sed -E "s/^(.{0})(.{4})/\1/" | tr "\n" "," | sed -E "s/,/\",\"/g"` 
+  PATTERNFLY_DEPS="[\"${LINES%???}\"]"
+else
+  PATTERNFLY_DEPS="[]"
+fi
+
+if [[ -n "$APP_BUILD_DIR" &&  -d $APP_BUILD_DIR ]]
+then
+    cd $APP_BUILD_DIR
+else
+    cd dist || cd build
+fi
+
+echo $NPM_INFO > ./app.info.deps.json
+
+echo "{
+  \"app_name\": \"$COMPONENT_NAME\",
+  \"src_hash\": \"$GIT_COMMIT\",
+  \"patternfly_dependencies\": $PATTERNFLY_DEPS,
+}" > ./app.info.json
+
+
+PREFIX=""
+if [[ "${TRAVIS_BRANCH}" = "master" ||  "${TRAVIS_BRANCH}" = "main" || "${TRAVIS_BRANCH}" =~ "beta" ]]; then
+  PREFIX="/beta"
+fi
+
+echo "server { 
+ listen 80;
+ server_name $COMPONENT_NAME;
+
+ location / {
+  try_files \$uri \$uri/ $PREFIX/apps/chrome/index.html;
+ }
+
+ location $PREFIX/apps/$COMPONENT_NAME {
+   alias /usr/share/nginx/html;
+ }
+}
+" > ./nginx.conf
+
+
+# Get bonfire helper scripts
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+# build the PR commit image
+source $CICD_ROOT/build.sh
