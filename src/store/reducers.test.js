d4ec98043984bf5a2cba6ce35039ecb7eb3aad14
rvsia <r.vsia@seznam.cz>
2021-09-14 15:44:47 +0200

    Add lastDateRequest check to onEntitiesLoaded

--- src/store/reducers.test.js
+++ src/store/reducers.test.js
@@ -1,8 +1,50 @@
+import { INVENTORY_ACTION_TYPES } from './action-types';
 import { tableReducer, entitesDetailReducer } from './reducers';
 
 describe('tableReducer', () => {
     test('should show default state', () => {
         expect(tableReducer(undefined, {})).toMatchObject({ loaded: false });
+    });
+
+    it('by default merges results', () => {
+        expect(
+            tableReducer(
+                {},
+                { type: INVENTORY_ACTION_TYPES.LOAD_ENTITIES_FULFILLED, payload: { results: [{ id: '123' }] } }
+            ))
+        .toEqual(
+            { rows: [{ id: '123', selected: undefined }]  }
+        );
+    });
+
+    it('merges results when the request is newer than state', () => {
+        expect(
+            tableReducer(
+                { lastDateRequest: 1 },
+                {
+                    type: INVENTORY_ACTION_TYPES.LOAD_ENTITIES_FULFILLED,
+                    payload: { results: [{ id: '123' }] },
+                    meta: { lastDateRequest: 1 }
+                }
+            ))
+        .toEqual(
+            { lastDateRequest: 1, rows: [{ id: '123', selected: undefined }] }
+        );
+    });
+
+    it('does not merge results when the request is older than state', () => {
+        expect(
+            tableReducer(
+                { lastDateRequest: 2 },
+                {
+                    type: INVENTORY_ACTION_TYPES.LOAD_ENTITIES_FULFILLED,
+                    payload: { results: [{ id: '123' }] },
+                    meta: { lastDateRequest: 1 }
+                }
+            ))
+        .toEqual(
+            { lastDateRequest: 2 }
+        );
     });
 });
 
