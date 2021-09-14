dac3cfbab6f5592e821ee623e669fca3fb5f3bb4
bholifie <bholifie@redhat.com>
2021-09-14 13:02:28 -0500

    factor out nginx config

--- pr_check.sh
+++ pr_check.sh
@@ -34,49 +34,7 @@ npm run verify
 # Issue with upload
 # npx codecov
 
-NPM_INFO="undefined"
-PATTERNFLY_DEPS="undefined"
-if [[ -f package-lock.json ]] || [[ -f yarn.lock ]];
-then
-  LINES=`npm list --silent --depth=0 --production | grep @patternfly -i | sed -E "s/^(.{0})(.{4})/\1/" | tr "\n" "," | sed -E "s/,/\",\"/g"` 
-  PATTERNFLY_DEPS="[\"${LINES%???}\"]"
-else PATTERNFLY_DEPS="[]" 
-fi
-
-if [[ -n "$APP_BUILD_DIR" &&  -d $APP_BUILD_DIR ]]
-then
-    cd $APP_BUILD_DIR
-else
-    cd dist || cd build
-fi
-
-echo $NPM_INFO > ./app.info.deps.json
-
-echo "{
-  \"app_name\": \"$APP_NAME\",
-  \"src_hash\": \"$GIT_COMMIT\",
-  \"patternfly_dependencies\": $PATTERNFLY_DEPS,
-}" > $APP_ROOT/app.info.json
-
-
-PREFIX=""
-if [[ "${TRAVIS_BRANCH}" = "master" ||  "${TRAVIS_BRANCH}" = "main" || "${TRAVIS_BRANCH}" =~ "beta" ]]; then
-  PREFIX="/beta"
-fi
-
-echo "server { 
- listen 80;
- server_name $APP_NAME;
-
- location / {
-  try_files \$uri \$uri/ $PREFIX/apps/chrome/index.html;
- }
-
- location $PREFIX/apps/$APP_NAME {
-   alias /usr/share/nginx/html;
- }
-}
-" > $APP_ROOT/nginx.conf
+source $APP_ROOT/nginx_conf.sh
 
 if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
     echo "QUAY_USER and QUAY_TOKEN must be set"
