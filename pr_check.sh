640933fd3450e248f7fd3c73a5597758690bac08
bholifie <bholifie@redhat.com>
2022-01-21 14:39:05 -0600

    build inside container

--- pr_check.sh
+++ pr_check.sh
@@ -4,11 +4,10 @@
 # Export vars for helper scripts to use
 # --------------------------------------------
 # name of app-sre "application" folder this component lives in; needs to match for quay
-export COMPONENT="insights-inventory" 
-export APP_NAME=`node -e 'console.log(require("./package.json").insights.appname)'`
-export IMAGE="quay.io/cloudservices/$COMPONENT-frontend"
-export WORKSPACE=${WORKSPACE:-$APP_ROOT}  # if running in jenkins, use the build's workspace
+export COMPONENT="insights-inventory"
+export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
 export APP_ROOT=$(pwd)
+export NODE_BUILD_VERSION=12
 cat /etc/redhat-release
 COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
 
@@ -20,22 +19,9 @@ IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION=""
 
 set -ex
-
-# ---------------------------
-# Build and Publish to Quay
-# ---------------------------
-
-npm ci
-npm run verify
-# Issue with upload
-# npx codecov
-
-# Generate nginx config based on app name in package.json
-curl -sSL $COMMON_BUILDER/src/nginx_conf_gen.sh | bash -s 
-
-# Set pr check images to expire so they don't clog the repo
-echo "LABEL quay.expires-after=3d" >> $APP_ROOT/Dockerfile # tag expires in 3 days
-curl -sSL $COMMON_BUILDER/src/quay_push.sh | bash -s 
+# source is preferred to | bash -s in this case to avoid a subshell
+source <(curl -sSL $COMMON_BUILDER/src/frontend-build.sh)
+BUILD_RESULTS=$?
 
 # Stubbed out for now
 mkdir -p $WORKSPACE/artifacts
@@ -44,3 +30,6 @@ cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
     <testcase classname="dummy" name="dummytest"/>
 </testsuite>
 EOF
+
+# teardown_docker
+exit $BUILD_RESULTS
