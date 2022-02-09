888e8d4930ea7cfd81c492a235f8113185b26cb0
Gabi Podolnikova <91887553+gabipodolnikova@users.noreply.github.com>
2022-02-09 17:17:19 +0100

    Removes actions toggle when table is empty (#1490)
    * Removes actions toggle when table is empty
    
    * forgotten let

--- src/components/InventoryTable/EntityTable.js
+++ src/components/InventoryTable/EntityTable.js
@@ -98,6 +98,10 @@ const EntityTable = ({
     };
 
     delete tableProps.RowWrapper;
+    if (rows?.length === 0) {
+        delete tableProps.actionResolver;
+    }
+
     return (
         <React.Fragment>
             { loaded && cells ?
@@ -173,7 +177,8 @@ EntityTable.propTypes = {
     tableProps: PropTypes.shape({
         [PropTypes.string]: PropTypes.any,
         RowWrapper: PropTypes.elementType,
-        variant: PropTypes.string
+        variant: PropTypes.string,
+        actionResolver: PropTypes.func
     }),
     onRowClick: PropTypes.func,
     showTags: PropTypes.bool,
