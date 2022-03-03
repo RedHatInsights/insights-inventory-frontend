4080865bfcbc08e1da0213d53d51f1229b8c47c5
Sebastian Gräßl <bastilian@users.noreply.github.com>
2022-03-03 11:51:31 +0100

    chore: Bump node to v16 (#1511)

--- src/__mocks__/hostApi.js
+++ src/__mocks__/hostApi.js
@@ -1,5 +1,5 @@
 import { hosts, tags } from '../api/api';
 import MockAdapter from 'axios-mock-adapter';
 
-export const mock = new MockAdapter(hosts.axios);
-export const mockTags = new MockAdapter(tags.axios);
+export const mock = new MockAdapter(hosts.axios, { onNoMatch: 'throwException' });
+export const mockTags = new MockAdapter(tags.axios, { onNoMatch: 'throwException' });
