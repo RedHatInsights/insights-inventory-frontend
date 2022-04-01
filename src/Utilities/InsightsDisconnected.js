cd09540bde0cb673acaf9d5ce6e7bb118d67d6c0
mkholjur <mkholjur@redhat.com>
2022-04-01 12:31:58 +0200

    ESSNTL-2118: Insights tether CTA

--- src/Utilities/InsightsDisconnected.js
+++ src/Utilities/InsightsDisconnected.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
-import { Tooltip } from '@patternfly/react-core';
+import { Tooltip, Grid, GridItem } from '@patternfly/react-core';
 import { DisconnectedIcon } from '@patternfly/react-icons';
 
 import './InsightsDisconnected.scss';
 
 const InsightsDisconnected = () => (
-    <Tooltip content="Insights disabled">
+    <Tooltip
+        maxWidth='14rem'
+        content={(
+            <Grid hasGutter>
+                <GridItem>
+                    Insights client not reporting
+                </GridItem>
+                <GridItem>
+                    From the main navigation, open
+                    &quot;Registration Assitant&quot; to learn
+                    how to set up Insights.
+                </GridItem>
+            </Grid>
+        )}>
         <span className="pf-u-ml-sm ins-c-inventor__disconnected-icon">
             <DisconnectedIcon />
         </span>
---
+++ src/components/InventoryDetail/InsightsPrompt.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+    Alert, Grid, GridItem, Text, TextContent, TextVariants, TextList, TextListItem,
+    Card, CardTitle, CardFooter, CardBody, ClipboardCopy, Button } from '@patternfly/react-core';
+
+const InsightsPrompt = () => {
+    return (
+        <Alert variant="info" isInline title="Your Insights-client is not repotring">
+            <Grid>
+                <GridItem>
+                    <Grid hasGutter>
+                        <GridItem span={4}>
+                            <TextContent style={{ '--pf-c-content--LineHeight': '1', '--pf-c-content--FontSize': '0.9rem' }}>
+                                <Text component={TextVariants.p}>
+                                    With Insights you can easly:
+                                </Text>
+                                <TextList
+                                    component='ul'
+                                    style={{ '--pf-c-content--ul--PaddingLeft': '0', '--pf-c-content--ul--MarginLeft': '1rem' }}
+                                >
+                                    <TextListItem>Optimize your IT Operations</TextListItem>
+                                    <TextListItem>Manage your security and compliance</TextListItem>
+                                    <TextListItem>Resolve operational issues quickly</TextListItem>
+                                </TextList>
+                            </TextContent>
+                        </GridItem>
+                        <GridItem span={4}>
+                            <Card isCompact>
+                                <CardTitle>Client setup</CardTitle>
+                                <CardBody>Make sure the client is properly installed and registered</CardBody>
+                                <CardFooter>
+                                    <ClipboardCopy isCode isReadOnly variant={'expansion'}>
+                                        {'yum -install insights \ninsights-client register'}
+                                    </ClipboardCopy>
+                                </CardFooter>
+                            </Card>
+                        </GridItem>
+                        <GridItem span={4}>
+                            <Card isCompact style={{ height: '100%' }}>
+                                <CardTitle>Configure troubleshooting</CardTitle>
+                                <CardBody>Proceed with verification on the client to ensure insights-client is
+                                    properly running and collecting data</CardBody>
+                                <CardFooter>
+                                    <Button component="a" target="_blank"
+                                        variant="link" href='https://access.redhat.com/solutions/6758841' isInline
+                                    >
+                                        Host not reporting data to Red Hat Insights
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        </GridItem>
+                    </Grid>
+                </GridItem>
+                <GridItem>
+                    <Button component="a" target="_blank" variant="link"
+                        href='https://console.redhat.com/security/insights' isInline
+                    >
+                        How does Red Hat keep data secure?
+                    </Button>
+                </GridItem>
+            </Grid>
+        </Alert>
+    );
+};
+
+export default InsightsPrompt;
