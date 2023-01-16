b246b3672936b72d6149b59da4acebc9f2978037
mkholjur <mkholjur@redhat.com>
2023-01-16 12:19:02 +0100

    fix(ESSNTL-4149): fix applying default filters from URL, with some cleanup

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,8 @@
 import { Route, Redirect, Switch } from 'react-router-dom';
 import React, { lazy, Suspense } from 'react';
-import { tagsMapper } from './constants';
-import { RHCD_FILTER_KEY, UPDATE_METHOD_KEY } from './Utilities/constants';
+import { getSearchParams } from './constants';
 import RenderWrapper from './Utilities/Wrapper';
+
 const InventoryTable = lazy(() => import('./routes/InventoryTable'));
 const InventoryDetail = lazy(() => import('./routes/InventoryDetail'));
 
@@ -13,26 +13,19 @@ export const routes = {
 };
 
 export const Routes = () => {
-    const searchParams = new URLSearchParams(location.search);
-
+    const searchParams = getSearchParams();
     return (
         <Suspense fallback="">
             <Switch>
                 <Route
                     exact
                     path={routes.table}
-                    render={() => <RenderWrapper
-                        cmp={InventoryTable}
-                        status={searchParams.getAll('status')}
-                        source={searchParams.getAll('source')}
-                        filterbyName={searchParams.getAll('hostname_or_id')}
-                        tagsFilter={searchParams.getAll('tags')?.[0]?.split?.(',').reduce?.(tagsMapper, [])}
-                        operatingSystem={searchParams.getAll('operating_system')}
-                        rhcdFilter={searchParams.getAll(RHCD_FILTER_KEY)}
-                        updateMethodFilter={searchParams.getAll(UPDATE_METHOD_KEY)}
-                        page={searchParams.getAll('page')}
-                        perPage={searchParams.getAll('per_page')}/>
-                    }
+                    render={() =>
+                        <RenderWrapper
+                            cmp={InventoryTable}
+                            isRbacEnabled
+                            {...searchParams}
+                        />}
                     rootClass='inventory'
                 />
                 <Route exact path={routes.detailWithModal} component={InventoryDetail} rootClass='inventory' />
