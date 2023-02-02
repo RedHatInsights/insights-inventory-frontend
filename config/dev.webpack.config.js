6e3639b7754a7bf473defa2b55c02da5a179834f
Georgy Karataev <georgkarat@gmail.com>
2023-02-02 10:05:31 +0100

    chore(ESSNTL-4219): Run with mocked API server (#1753)
    Implements https://issues.redhat.com/browse/ESSNTL-4219.
    
    This provides another local deployment option where webpack server
    starts together with the mocked API server. The API server is built on
    top of OpenAPI schema.

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -34,6 +34,20 @@ const { config: webpackConfig, plugins } = config({
                 }, {})
             }
         }
+    },
+    ...process.env.MOCK && {
+        customProxy: [
+            {
+                context: ['/api/inventory/v1/groups'], // you can adjust the `context` value to redirect only specific endpoints
+                target: 'http://localhost:4010', // default prism port
+                secure: false,
+                changeOrigin: true,
+                pathRewrite: { '^/api/inventory/v1': '' },
+                onProxyReq: (proxyReq) => {
+                    proxyReq.setHeader('x-rh-identity', 'foobar'); // avoid 401 errors by providing neccessary security header
+                }
+            }
+        ]
     }
 });
 
