b6b54aea7a2ce1bfc9995d888b784286373687bb
Aleksandr Voznesenskii <avoznese@redhat.com>
2023-03-09 12:48:57 +0100

    feat(inventory groups): adding fetch and redux store

--- src/components/filters/useGroupFilter.js
+++ src/components/filters/useGroupFilter.js
@@ -1,7 +1,8 @@
 /* eslint-disable camelcase */
 import { union } from 'lodash';
 import { useEffect, useMemo, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGroups } from '../../store/inventory-actions';
 import { HOST_GROUP_CHIP } from '../../Utilities/index';
 
 //for attaching this filter to the redux
@@ -12,49 +13,6 @@ export const groupFilterReducer = (_state, { type, payload }) => ({
         groupHostFilter: payload
     }
 });
-
-const mockApiResponse = [
-    {
-        created_at: '2023-02-28T12:35:20.071Z',
-        host_ids: [
-            'bA6deCFc19564430AB814bf8F70E8cEf'
-        ],
-        id: 'bA6deCFc19564430AB814bf8F70E8cEf',
-        name: 'sre-group0',
-        org_id: '000102',
-        updated_at: '2023-02-28T12:35:20.071Z'
-    },
-    {
-        created_at: '2023-02-28T12:35:20.071Z',
-        host_ids: [
-            'bA6deCFc19564430AB814bf8F70E8cEf'
-        ],
-        id: 'bA6deCFc19564430AB814bf8F70E8cEf',
-        name: 'sre-group1',
-        org_id: '000102',
-        updated_at: '2023-02-28T12:35:20.071Z'
-    },
-    {
-        created_at: '2023-02-28T12:35:20.071Z',
-        host_ids: [
-            'bA6deCFc19564430AB814bf8F70E8cEf'
-        ],
-        id: 'bA6deCFc19564430AB814bf8F70E8cEf',
-        name: 'sre-group2',
-        org_id: '000102',
-        updated_at: '2023-02-28T12:35:20.071Z'
-    },
-    {
-        created_at: '2023-02-28T12:35:20.071Z',
-        host_ids: [
-            'bA6deCFc19564430AB814bf8F70E8cEf'
-        ],
-        id: 'bA6deCFc19564430AB814bf8F70E8cEf',
-        name: 'sre-group3',
-        org_id: '000102',
-        updated_at: '2023-02-28T12:35:20.071Z'
-    }
-];
 
 //receive the array of selected groups and return chips based on the name of selected groups
 export const buildHostGroupChips = (selectedGroups = []) => {
@@ -71,27 +29,31 @@ export const buildHostGroupChips = (selectedGroups = []) => {
         : [];
 };
 
-const useGroupFilter = () => {
-    //currently mockApiResponse is replacing a proper API response
-    //buildHostGroupsValues build an array of objects to populate dropdown
-    const buildHostGroupsValues = mockApiResponse.reduce((acc, group) => {
-        acc.push({ label: group.name, value: group.name });
-        return acc;
+const useGroupFilter = (apiParams = []) => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchGroups(apiParams));
     }, []);
+    //fetched values
+    const fetchedValues = useSelector(({ groups })  => groups?.data?.results);
     //selected are the groups we selected
     const [selected, setSelected] = useState([]);
-    //host group values are all of the host group values available to the user
-    const [hostGroupValue, setHostGroupValue] = useState(buildHostGroupsValues);
-
+    //buildHostGroupsValues build an array of objects to populate dropdown
+    const [buildHostGroupsValues, setBuildHostGroupsValues] = useState([]);
+    //hostGroupValue is used for config items
+    useEffect(() => {
+        setBuildHostGroupsValues(fetchedValues?.reduce((acc, group) => {
+            acc.push({ label: group.name, value: group.name });
+            return acc;
+        }, []));
+    }, [fetchedValues]);
+    //this is used in the filter config as a way to select values onChange
     const onHostGroupsChange = (event, selection) => {
         setSelected(union(selected, selection));
     };
 
-    const hostGroupsLoaded = useSelector(({ entities }) => entities?.groups);
-    useEffect(() => {
-        setHostGroupValue(hostGroupsLoaded);
-    }, []);
     const chips = useMemo(() => buildHostGroupChips(selected), [selected]);
+    //chips that is built for the filter config
 
     //hostGroupConfig is a config that we use in EntityTableToolbar.js
     const hostGroupConfig = useMemo(() => ({
@@ -105,8 +67,9 @@ const useGroupFilter = () => {
             value: selected,
             items: buildHostGroupsValues
         }
-    }), [selected, hostGroupValue]);
+    }), [selected, buildHostGroupsValues]);
 
+    //setSelectedValues is used for selecting and deleting values
     const setSelectedValues = (currentValue = []) => {
         setSelected(currentValue);
     };
--- src/components/filters/useGroupFilter.test.js
+++ src/components/filters/useGroupFilter.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { waitFor } from '@testing-library/react';
 import { act, renderHook } from '@testing-library/react-hooks';
 import { Provider } from 'react-redux';
@@ -38,24 +39,104 @@ describe('useGroupFilter', () => {
             <Provider
                 store={mockStore({
                     entities: {
-                        groups: [
-                            {
-                                label: 'sre-group0',
-                                value: 'sre-group0'
-                            },
-                            {
-                                label: 'sre-group1',
-                                value: 'sre-group1'
-                            },
-                            {
-                                label: 'sre-group2',
-                                value: 'sre-group2'
-                            },
-                            {
-                                label: 'sre-group3',
-                                value: 'sre-group3'
+                        groups: {
+                            data: {
+                                page: 1,
+                                count: 50,
+                                results: [{
+                                    created_at: '2019-02-18T23:00:00.0Z',
+                                    id: '4f5B88dBe1D4eB732B388abc1Baa4BAc',
+                                    updated_at: '1960-11-06T23:00:00.0Z',
+                                    org_id: 'nostrud in deserunt',
+                                    account: 'ullamco dolore pariatur sint',
+                                    host_ids: [
+                                        '5f20569C-9E96-C794-887c-1Cb5D6e220b2',
+                                        'D34e68beFc2d4fF1C19CF4CaF913d1b6',
+                                        '76D3AcBB-5B4C-8BB8-f3BB-69808CcFa84A',
+                                        '185dC76d-D6B8-059b-1bCb-685D2edd0CEa',
+                                        '50E0cAaDCace55e07AebC7Bcc40cfdAD',
+                                        'be8e0A09AdEE9a23fe65972F43D31bDA',
+                                        '8B6764001Bf6d0bA4E6aEdCAf207bf71',
+                                        '80CffFbd-04C9-cBB6-5Bf5-Dc0eBbDBba70',
+                                        '55d8323C-dEbC-B10e-f88e-23ba37068C9f',
+                                        'C5A8Fd55a0B6fa3A3a748cb0a4cc20BB',
+                                        '1dF767C34BF222E65BD46BcC3A62de4b',
+                                        '4d44747b1D8D1648EC5713e983984a6C',
+                                        'b89A20e9da89Eb9862502748ca9aa0bf',
+                                        'F24dc6A1548B40dF453FdBEa4DD5CbEB',
+                                        '2Ba0BCE6-F8cC-FAFE-Ed72-f7BF01dC60A4',
+                                        '1f255AB7-d49f-f8Ff-48eD-B9c6E4674Fe2',
+                                        '8277DD2f3Fc0aaE89D601400E86E4E86',
+                                        '3Be6b8FE-8B32-240d-9C0f-0c074baC5EBb',
+                                        '94c4bBCB31D0dfa37e955Dc94eAd3Dc0',
+                                        '2B0725BE87671E4DffE19F0f7fc8aFe7'
+                                    ],
+                                    name: 'nisi ut consequat ad'
+                                },
+                                {
+                                    created_at: '2019-02-18T23:00:00.0Z',
+                                    id: '4f5B88dBe1D4eB732B388abc1Baa4BAc',
+                                    updated_at: '1960-11-06T23:00:00.0Z',
+                                    org_id: 'nostrud in deserunt',
+                                    account: 'ullamco dolore pariatur sint',
+                                    host_ids: [
+                                        '5f20569C-9E96-C794-887c-1Cb5D6e220b2',
+                                        'D34e68beFc2d4fF1C19CF4CaF913d1b6',
+                                        '76D3AcBB-5B4C-8BB8-f3BB-69808CcFa84A',
+                                        '185dC76d-D6B8-059b-1bCb-685D2edd0CEa',
+                                        '50E0cAaDCace55e07AebC7Bcc40cfdAD',
+                                        'be8e0A09AdEE9a23fe65972F43D31bDA',
+                                        '8B6764001Bf6d0bA4E6aEdCAf207bf71',
+                                        '80CffFbd-04C9-cBB6-5Bf5-Dc0eBbDBba70',
+                                        '55d8323C-dEbC-B10e-f88e-23ba37068C9f',
+                                        'C5A8Fd55a0B6fa3A3a748cb0a4cc20BB',
+                                        '1dF767C34BF222E65BD46BcC3A62de4b',
+                                        '4d44747b1D8D1648EC5713e983984a6C',
+                                        'b89A20e9da89Eb9862502748ca9aa0bf',
+                                        'F24dc6A1548B40dF453FdBEa4DD5CbEB',
+                                        '2Ba0BCE6-F8cC-FAFE-Ed72-f7BF01dC60A4',
+                                        '1f255AB7-d49f-f8Ff-48eD-B9c6E4674Fe2',
+                                        '8277DD2f3Fc0aaE89D601400E86E4E86',
+                                        '3Be6b8FE-8B32-240d-9C0f-0c074baC5EBb',
+                                        '94c4bBCB31D0dfa37e955Dc94eAd3Dc0',
+                                        '2B0725BE87671E4DffE19F0f7fc8aFe7'
+                                    ],
+                                    name: 'nisi ut consequat ad1'
+                                },
+                                {
+                                    created_at: '2019-02-18T23:00:00.0Z',
+                                    id: '4f5B88dBe1D4eB732B388abc1Baa4BAc',
+                                    updated_at: '1960-11-06T23:00:00.0Z',
+                                    org_id: 'nostrud in deserunt',
+                                    account: 'ullamco dolore pariatur sint',
+                                    host_ids: [
+                                        '5f20569C-9E96-C794-887c-1Cb5D6e220b2',
+                                        'D34e68beFc2d4fF1C19CF4CaF913d1b6',
+                                        '76D3AcBB-5B4C-8BB8-f3BB-69808CcFa84A',
+                                        '185dC76d-D6B8-059b-1bCb-685D2edd0CEa',
+                                        '50E0cAaDCace55e07AebC7Bcc40cfdAD',
+                                        'be8e0A09AdEE9a23fe65972F43D31bDA',
+                                        '8B6764001Bf6d0bA4E6aEdCAf207bf71',
+                                        '80CffFbd-04C9-cBB6-5Bf5-Dc0eBbDBba70',
+                                        '55d8323C-dEbC-B10e-f88e-23ba37068C9f',
+                                        'C5A8Fd55a0B6fa3A3a748cb0a4cc20BB',
+                                        '1dF767C34BF222E65BD46BcC3A62de4b',
+                                        '4d44747b1D8D1648EC5713e983984a6C',
+                                        'b89A20e9da89Eb9862502748ca9aa0bf',
+                                        'F24dc6A1548B40dF453FdBEa4DD5CbEB',
+                                        '2Ba0BCE6-F8cC-FAFE-Ed72-f7BF01dC60A4',
+                                        '1f255AB7-d49f-f8Ff-48eD-B9c6E4674Fe2',
+                                        '8277DD2f3Fc0aaE89D601400E86E4E86',
+                                        '3Be6b8FE-8B32-240d-9C0f-0c074baC5EBb',
+                                        '94c4bBCB31D0dfa37e955Dc94eAd3Dc0',
+                                        '2B0725BE87671E4DffE19F0f7fc8aFe7'
+                                    ],
+                                    name: 'nisi ut consequat ad2'
+                                }
+                                ],
+                                total: 100
                             }
-                        ]
+                        }
                     }
                 })}
             >
@@ -67,32 +148,6 @@ describe('useGroupFilter', () => {
             const { result } = renderHook(useGroupFilter, { wrapper });
             expect(result.current).toMatchSnapshot();
         });
-
-        /*
-        This test should be rewritten when I connect group filters to the redux store
-        it('should return correct filter config', () => {
-            const { result } = renderHook(useGroupFilter, { wrapper });
-            const [config] = result.current;
-            expect(config.filterValues.items).toBe([
-                {
-                    label: 'sre-group0',
-                    value: 'sre-group0'
-                },
-                {
-                    label: 'sre-group1',
-                    value: 'sre-group1'
-                },
-                {
-                    label: 'sre-group2',
-                    value: 'sre-group2'
-                },
-                {
-                    label: 'sre-group3',
-                    value: 'sre-group3'
-                }
-            ]);
-            expect(config.label).toBe('Groups');
-        }); */
 
         it('should return correct chips array, current value and value setter', () => {
             const { result } = renderHook(useGroupFilter, { wrapper });
@@ -106,6 +161,29 @@ describe('useGroupFilter', () => {
             expect(chipsUpdated.length).toBe(1);
             expect(valueUpdated).toEqual(['Group1']);
             expect(chipsUpdated).toMatchSnapshot();
+        });
+
+        it('should return empty state value if no groups obtained', () => {
+            const wrapper = ({ children }) => (
+                <Provider
+                    store={mockStore({
+                        entities: {
+                            groups: {
+                                data: {
+                                    page: 1,
+                                    count: 50,
+                                    results: [],
+                                    total: 100
+                                }
+                            }
+                        }
+                    })}
+                >
+                    {children}
+                </Provider>
+            );
+            const { result } = renderHook(useGroupFilter, { wrapper });
+            expect(result.current).toMatchSnapshot();
         });
     });
 });
