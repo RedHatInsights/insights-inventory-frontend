55df9efb8e223d098f0c2f58939e6f318e9838ac
Georgy Karataev <georgkarat@gmail.com>
2023-03-30 17:03:05 +0200

    Fix GroupDetailInfo tests

--- src/components/InventoryGroupDetail/GroupDetailInfo.cy.js
+++ src/components/InventoryGroupDetail/GroupDetailInfo.cy.js
@@ -7,35 +7,20 @@ const mountPage = (params) =>
         <GroupDetailInfo {...params}/>
     );
 
-before(() => {
-    cy.window().then(
-        (window) =>
-            (window.insights = {
-                chrome: {
-                    isProd: false,
-                    auth: {
-                        getUser: () => {
-                            return Promise.resolve({});
-                        }
-                    }
-                }
-            })
-    );
-});
-
 describe('group detail information page', () => {
+    beforeEach(() => {
+        mountPage({ chrome: { isBeta: () => false } });
+    });
+
     it('title is rendered', () => {
-        mountPage();
         cy.get('div[class="pf-c-card__title pf-c-title pf-m-lg card-title"]').should('have.text', 'User access configuration');
     });
 
     it('button is present', () => {
-        mountPage();
-        cy.get('button[class="pf-c-button pf-m-secondary"]').should('have.text', 'Manage access');
+        cy.get('a[class="pf-c-button pf-m-secondary"]').should('have.text', 'Manage access');
     });
 
     it('card text is present', () => {
-        mountPage();
         cy.get('div[class="pf-c-card__body"]').should
         ('have.text', 'Manage your inventory group user access configuration under Identity & Access Management > User Access.');
     });
