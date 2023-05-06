77a37994ebf7884a96cb17bf51f8e04d1a197eee
acosferreia <acosferreira@gmail.com>
2023-05-05 18:04:33 -0700

    add federation config

---
+++ fec.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    appUrl: '/staging/starter',
+    debug: true,
+    useProxy: true,
+    proxyVerbose: true,
+    /**
+     * Change to false after your app is registered in configuration files
+     */
+    interceptChromeConfig: false,
+    /**
+     * Add additional webpack plugins
+     */
+    
+    
+  };
