5ba8cc626dd7cbc416e9187733b2dce19843feaa
Georgy Karataev <georgkarat@gmail.com>
2023-05-17 09:02:52 +0200

    Add some tests

---
+++ src/components/InventoryGroups/Modals/AddHostToGroupModal.cy.js
@@ -0,0 +1,43 @@
+import { groupsInterceptors } from '../../../../cypress/support/interceptors';
+import AddHostToGroupModal from './AddHostToGroupModal';
+
+const mountModal = (props =
+{
+    isModalOpen: true,
+    setIsModalOpen: () => {},
+    modalState: {
+        name: 'host1',
+        id: 'host1-id'
+    },
+    reloadData: () => {}
+}) => {
+    cy.mountWithContext(AddHostToGroupModal, {}, props);
+};
+
+before(() => {
+    cy.window().then(
+        (window) =>
+            (window.insights = {
+                chrome: {
+                    auth: {
+                        getUser: () => {
+                            return Promise.resolve({});
+                        }
+                    }
+                }
+            })
+    );
+});
+
+describe('AddHostToGroupModal', () => {
+    it('makes separate requests when searching groups', () => {
+        groupsInterceptors['successful with some items']();
+        mountModal();
+
+        cy.wait('@getGroups'); // must make initial call
+        cy.get('input').type('abc');
+        cy.wait('@getGroups').its('request.url').should('contain', '?name=abc');
+        cy.get('input').type('d');
+        cy.wait('@getGroups').its('request.url').should('contain', '?name=abcd');
+    });
+});
