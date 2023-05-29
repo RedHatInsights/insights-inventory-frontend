b7518d3ecbc44aae5e71fd1acaf72922952b7298
Georgy Karataev <georgkarat@gmail.com>
2023-05-29 13:09:21 +0200

    Rename the removal modal component

--- src/routes/InventoryTable.js
+++ src/routes/InventoryTable.js
@@ -17,7 +17,7 @@ import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 import AddSelectedHostsToGroupModal from '../components/InventoryGroups/Modals/AddSelectedHostsToGroupModal';
 import useFeatureFlag from '../Utilities/useFeatureFlag';
 import { useBulkSelectConfig } from '../Utilities/hooks/useBulkSelectConfig';
-import RemoveSelectedHostsFromGroupModal from '../components/InventoryGroups/Modals/RemoveSelectedHostsFromGroupModal';
+import RemoveHostsFromGroupModal from '../components/InventoryGroups/Modals/RemoveHostsFromGroupModal';
 
 const mapTags = ({ category, values }) => values.map(({ tagKey, value }) => `${
     category ? `${category}/` : ''
@@ -351,7 +351,7 @@ const Inventory = ({
                         }
                         {
                             removeHostsFromGroupModalOpen &&
-                            <RemoveSelectedHostsFromGroupModal
+                            <RemoveHostsFromGroupModal
                                 isModalOpen={removeHostsFromGroupModalOpen}
                                 setIsModalOpen={setRemoveHostsFromGroupModalOpen}
                                 modalState={currentSystem}
