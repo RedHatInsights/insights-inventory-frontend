ae69ccb46347b36265b57b4ac4ea3baf56430e16
Georgy Karataev <georgkarat@gmail.com>
2023-05-31 17:19:11 +0200

    Support multiple values in URLSearchParams utils

--- src/Utilities/URLSearchParams.js
+++ src/Utilities/URLSearchParams.js
@@ -3,14 +3,24 @@
  * The functions require window.location and window.history to be available.
  */
 export const updateURLSearchParams = (filters = {}, filtersConfig) => {
-    const filtersVerified = Object.fromEntries(
-        Object.entries(filters)
-        .filter(([key]) => filtersConfig[key] !== undefined)
-        .map(([key, value]) => {
-            const { paramName, transformToParam } = filtersConfig[key];
-            return [paramName, transformToParam ? transformToParam(value) : value];
-        })
-    );
+    const filtersVerified = [];
+
+    Object.entries(filters)
+    .filter(([key]) => filtersConfig[key] !== undefined)
+    .forEach(([key, value]) => {
+        const { paramName, transformToParam, isSeparated } = filtersConfig[key];
+        const paramValue = transformToParam ? transformToParam(value) : value;
+
+        if (isSeparated) {
+            // paramValue must be an array in that case
+            paramValue.forEach((item) => {
+                filtersVerified.push([paramName, item]);
+            });
+        } else {
+            filtersVerified.push([paramName, paramValue]);
+        }
+    });
+
     const newSearch = new URLSearchParams(filtersVerified).toString();
     const newPathname = window.location.pathname + '?' + newSearch;
     window.history.replaceState(null, '', newPathname);
@@ -18,22 +28,33 @@ export const updateURLSearchParams = (filters = {}, filtersConfig) => {
 
 export const readURLSearchParams = (params = '', filtersConfig) => {
     const searchParams = new URLSearchParams(params);
-    const filtersVerified = Array.from(searchParams.entries())
+
+    const outputFilters = {};
+
+    Array.from(searchParams.entries())
     .filter(([key]) =>
         // check whether the parameter is present in the filters configuration
         Object.values(filtersConfig).find(({ paramName }) => paramName === key)
     )
-    .map(([key, value]) => {
+    .forEach(([key, value]) => {
         // transform back to the filters representation
-        const [filterName, { transformFromParam }] = Object.entries(
+        const [filterName, { transformFromParam, isSeparated }] = Object.entries(
             filtersConfig
         ).find(([, config]) => config.paramName === key);
 
-        return [
-            filterName,
-            transformFromParam ? transformFromParam(value) : value
-        ];
+        const finalValue  = transformFromParam ? transformFromParam(value) : value;
+
+        if (isSeparated === true) {
+            // means this is an array of values corresponding to the same parameter
+            if (outputFilters[filterName] !== undefined) {
+                outputFilters[filterName].push(finalValue);
+            } else {
+                outputFilters[filterName] = [finalValue];
+            }
+        } else {
+            outputFilters[filterName] = finalValue;
+        }
     });
 
-    return Object.fromEntries(filtersVerified);
+    return outputFilters;
 };
--- src/Utilities/URLSearchParams.test.js
+++ src/Utilities/URLSearchParams.test.js
@@ -27,13 +27,23 @@ describe('URLSearchParams', () => {
             });
             expect(window.history.replaceState).toHaveBeenCalledWith(null, '', '/?name=123_param');
         });
+
+        it('separate values when needed', () => {
+            updateURLSearchParams({
+                name: ['123', '456']
+            }, {
+                name: {
+                    paramName: 'name',
+                    isSeparated: true
+                }
+            });
+            expect(window.history.replaceState).toHaveBeenCalledWith(null, '', '/?name=123&name=456');
+        });
     });
 
     describe('readURLSearchParams', () => {
         it('simple parameters', () => {
-            expect(readURLSearchParams({
-                name: '123'
-            }, {
+            expect(readURLSearchParams('?name=123', {
                 nameFilter: {
                     paramName: 'name'
                 }
@@ -43,15 +53,24 @@ describe('URLSearchParams', () => {
         });
 
         it('with transform function', () => {
-            expect(readURLSearchParams({
-                name: '123'
-            }, {
+            expect(readURLSearchParams('?name=123', {
                 nameFilter: {
                     paramName: 'name',
                     transformFromParam: (value) => parseInt(value)
                 }
             })).toEqual({
                 nameFilter: 123
+            });
+        });
+
+        it('recognizes multiple values', () => {
+            expect(readURLSearchParams('?name=123&name=456', {
+                nameFilter: {
+                    paramName: 'name',
+                    isSeparated: true
+
+                } })).toEqual({
+                nameFilter: ['123', '456']
             });
         });
     });
