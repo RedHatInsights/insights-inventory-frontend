82f439e7552d44260b37f2a9c16cee63dcf2f3ef
Georgy Karataev <georgkarat@gmail.com>
2023-07-10 11:08:08 +0200

    feat(ESSNTL-5052): Disable create group button if not authorized (#1926)
    Implements https://issues.redhat.com/browse/ESSNTL-5052.

--- src/components/InventoryGroups/SmallComponents/CreateGroupButton.js
+++ src/components/InventoryGroups/SmallComponents/CreateGroupButton.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { Button, Text } from '@patternfly/react-core';
+import { Button, Text, Tooltip } from '@patternfly/react-core';
 import PropTypes from 'prop-types';
+import { usePermissionsWithContext } from '@redhat-cloud-services/frontend-components-utilities/RBACHook';
+import { NO_MODIFY_GROUPS_TOOLTIP_MESSAGE } from '../../../constants';
 
-export const CreateGroupButton = ({ closeModal }) => (
-  <>
-    <Text>Or</Text>
-    <Button variant="secondary" className="pf-u-w-50" onClick={closeModal}>
-      Create a new group
-    </Button>
-  </>
-);
+export const CreateGroupButton = ({ closeModal }) => {
+  const { hasAccess: canModifyGroups } = usePermissionsWithContext([
+    'inventory:groups:write',
+  ]);
+
+  return (
+    <>
+      <Text>Or</Text>
+      {canModifyGroups ? (
+        <Button variant="secondary" className="pf-u-w-50" onClick={closeModal}>
+          Create a new group
+        </Button>
+      ) : (
+        <Tooltip content={NO_MODIFY_GROUPS_TOOLTIP_MESSAGE}>
+          <Button variant="secondary" className="pf-u-w-50" isAriaDisabled>
+            Create a new group
+          </Button>
+        </Tooltip>
+      )}
+    </>
+  );
+};
 
 CreateGroupButton.propTypes = {
   closeModal: PropTypes.func,
