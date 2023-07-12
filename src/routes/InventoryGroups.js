377ae9c9b32a5f171338f74451187fde35bda57f
Georgy Karataev <georgkarat@gmail.com>
2023-07-12 12:38:31 +0200

    Check for hosts:read before showing group hosts

--- src/routes/InventoryGroups.js
+++ src/routes/InventoryGroups.js
@@ -2,13 +2,12 @@ import React, { useEffect } from 'react';
 import InventoryGroups from '../components/InventoryGroups';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 import { usePermissionsWithContext } from '@redhat-cloud-services/frontend-components-utilities/RBACHook';
-import AccessDenied from '../Utilities/AccessDenied';
-import { EmptyStateVariant } from '@patternfly/react-core';
 import {
   PageHeader,
   PageHeaderTitle,
 } from '@redhat-cloud-services/frontend-components/PageHeader';
 import { GENERAL_GROUPS_READ_PERMISSION } from '../constants';
+import { EmptyStateNoAccessToGroups } from '../components/InventoryGroupDetail/EmptyStateNoAccess';
 
 const REQUIRED_PERMISSIONS = [GENERAL_GROUPS_READ_PERMISSION];
 
@@ -25,22 +24,7 @@ const Groups = () => {
       <PageHeader>
         <PageHeaderTitle title="Groups" />
       </PageHeader>
-      {hasAccess ? (
-        <InventoryGroups />
-      ) : (
-        <AccessDenied
-          title="Inventory group access permissions needed"
-          showReturnButton={false}
-          description={
-            <div>
-              You do not have the necessary inventory group permissions to see
-              inventory groups. Contact your organization administrator for
-              access.
-            </div>
-          }
-          variant={EmptyStateVariant.large} // overrides the default "full" value
-        />
-      )}
+      {hasAccess ? <InventoryGroups /> : <EmptyStateNoAccessToGroups />}
     </React.Fragment>
   );
 };
