7e761b2197de23fee567d44e979c8fc765d984f2
Adelia Ferreira <adferrei@redhat.com>
2023-07-17 10:12:00 +0100

    fix(THEEDGE-3466): Add validation to just show image details when image is hosted by edge (#1937)
    Implements https://issues.redhat.com/browse/THEEDGE-3466.
    
    Signed-off-by: Adelia Ferreira <adferrei@redhat.com>

--- src/components/SystemDetails/GeneralInfo.js
+++ src/components/SystemDetails/GeneralInfo.js
@@ -1,21 +1,26 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import GeneralInformation from '../GeneralInfo/GeneralInformation';
 import useFeatureFlag from '../../Utilities/useFeatureFlag';
 export { default as TextInputModal } from '../GeneralInfo/TextInputModal';
 
 const GeneralInfoTab = (props) => {
+  const systemProfile = useSelector(
+    ({ systemProfileStore }) => systemProfileStore?.systemProfile
+  );
+  const isEdgeHost = systemProfile?.host_type === 'edge';
   const enableEdgeImageDetails = useFeatureFlag(
     'edgeParity.inventory-system-detail'
   );
   const enableEdgeInventoryListDetails = useFeatureFlag(
     'edgeParity.inventory-list'
   );
+
   return (
     <GeneralInformation
       {...props}
       showImageDetails={
-        enableEdgeImageDetails && enableEdgeInventoryListDetails
+        enableEdgeImageDetails && enableEdgeInventoryListDetails && isEdgeHost
       }
     />
   );
