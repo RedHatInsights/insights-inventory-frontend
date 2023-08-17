cd00a7410fa53dbaab28cfca648ae46866f03449
Georgy Karataev <georgkarat@gmail.com>
2023-08-17 14:10:42 +0200

    fix(ESSNTL-4667): Disable global filter at groups views (#1985)
    Fixes https://issues.redhat.com/browse/ESSNTL-4667.

--- src/components/InventoryGroupDetail/index.js
+++ src/components/InventoryGroupDetail/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import InventoryGroupDetail from './InventoryGroupDetail';
 
 const InventoryGroupDetailWrapper = () => {
   const { groupId } = useParams();
+  const chrome = useChrome();
+
+  useEffect(() => {
+    chrome?.hideGlobalFilter?.();
+  }, []);
 
   return <InventoryGroupDetail groupId={groupId} />;
 };
