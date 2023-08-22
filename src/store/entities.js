10bdab00fe609b3de6f985f8aee50f12f48d3d6b
leSamo <soleksak@redhat.com>
2023-08-22 18:03:08 +0200

    Roll back some unnecessary data labels

--- src/store/entities.js
+++ src/store/entities.js
@@ -45,7 +45,6 @@ export const defaultColumns = (groupsEnabled = false) => [
     key: 'display_name',
     sortKey: 'display_name',
     title: 'Name',
-    dataLabel: 'Name',
     renderFunc: TitleColumn,
   },
   ...(groupsEnabled
@@ -54,7 +53,6 @@ export const defaultColumns = (groupsEnabled = false) => [
           key: 'groups',
           sortKey: 'groups',
           title: 'Group',
-          dataLabel: 'Group',
           props: { width: 10, isStatic: true },
           // eslint-disable-next-line camelcase
           renderFunc: (groups) => (isEmpty(groups) ? 'N/A' : groups[0].name), // currently, one group at maximum is supported
@@ -65,7 +63,6 @@ export const defaultColumns = (groupsEnabled = false) => [
   {
     key: 'tags',
     title: 'Tags',
-    dataLabel: 'Tags',
     props: { width: 10, isStatic: true },
     // eslint-disable-next-line react/display-name
     renderFunc: (value, systemId) => (
@@ -93,7 +90,6 @@ export const defaultColumns = (groupsEnabled = false) => [
     key: 'updated',
     sortKey: 'updated',
     title: 'Last seen',
-    dataLabel: 'Last seen',
     // eslint-disable-next-line react/display-name
     renderFunc: (
       value,
