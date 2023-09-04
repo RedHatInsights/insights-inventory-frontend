91eb69483bb95a8ba2cbee9158ac736c43112624
Georgy Karataev <georgkarat@gmail.com>
2023-09-04 10:48:30 +0200

    fix(RHIF-282): Show helping expandable at /groups (#2011)
    Implements https://issues.redhat.com/browse/RHIF-282.

--- src/components/InventoryGroups/InventoryGroups.js
+++ src/components/InventoryGroups/InventoryGroups.js
@@ -1,21 +1,26 @@
 import ErrorState from '@redhat-cloud-services/frontend-components/ErrorState';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import { Bullseye, Spinner } from '@patternfly/react-core';
 import GroupsTable from '../GroupsTable/GroupsTable';
 import { getGroups } from '../InventoryGroups/utils/api';
 import NoGroupsEmptyState from './NoGroupsEmptyState';
 import { EmptyStateNoAccessToGroups } from '../InventoryGroupDetail/EmptyStateNoAccess';
+import GetHelpExpandable from './GetHelpExpandable';
+import CreateGroupModal from './Modals/CreateGroupModal';
 
 const InventoryGroups = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [hasGroups, setHasGroups] = useState(false);
   const [hasError, setHasError] = useState(false);
   const [error, setError] = useState(null);
+  const [createModalOpen, setCreateModalOpen] = useState(false);
 
   const ignore = useRef(false); // https://react.dev/learn/synchronizing-with-effects#fetching-data
 
   const handleLoading = async () => {
+    setIsLoading(true);
+
     // make initial request to check if there is at least one group available
     try {
       const { total } = await getGroups();
@@ -33,6 +38,10 @@ const InventoryGroups = () => {
     !ignore.current && setIsLoading(false);
   };
 
+  const onCreateGroupClick = useCallback(() => {
+    setCreateModalOpen(true);
+  }, []);
+
   useEffect(() => {
     handleLoading();
 
@@ -45,7 +54,16 @@ const InventoryGroups = () => {
     <section
       className="pf-l-page__main-section pf-c-page__main-section"
       data-ouia-component-id="groups-table-wrapper"
+      style={{ height: '100%' }}
     >
+      {createModalOpen && (
+        <CreateGroupModal
+          isModalOpen={createModalOpen}
+          setIsModalOpen={setCreateModalOpen}
+          reloadData={handleLoading}
+        />
+      )}
+      <GetHelpExpandable />
       {hasError ? (
         error?.status === 403 || error?.response?.status === 403 ? (
           <EmptyStateNoAccessToGroups />
@@ -57,9 +75,9 @@ const InventoryGroups = () => {
           <Spinner />
         </Bullseye>
       ) : hasGroups ? (
-        <GroupsTable />
+        <GroupsTable onCreateGroupClick={onCreateGroupClick} />
       ) : (
-        <NoGroupsEmptyState reloadData={handleLoading} />
+        <NoGroupsEmptyState onCreateGroupClick={onCreateGroupClick} />
       )}
     </section>
   );
--- src/routes/InventoryGroups.test.js
+++ src/routes/InventoryGroups.test.js
@@ -3,6 +3,21 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import InventoryGroups from './InventoryGroups';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn(),
+}));
+
+jest.mock(
+  '@redhat-cloud-services/frontend-components-utilities/RBACHook',
+  () => ({
+    ...jest.requireActual(
+      '@redhat-cloud-services/frontend-components-utilities/RBACHook'
+    ),
+    usePermissionsWithContext: () => ({ hasAccess: true }),
+  })
+);
+
 describe('inventory groups route', () => {
   it('renders header and table wrapper', () => {
     const { container } = render(<InventoryGroups />);
@@ -10,6 +25,15 @@ describe('inventory groups route', () => {
     expect(container.querySelector('h1')).toHaveTextContent('Groups');
     expect(
       container.querySelector('[data-ouia-component-id="groups-table-wrapper"]')
+    ).toBeInTheDocument();
+  });
+
+  it('should contain get help expandable', () => {
+    const { getByText } = render(<InventoryGroups />);
+    expect(getByText('Help get started with new features')).toBeInTheDocument();
+    expect(getByText('Create an Inventory group')).toBeInTheDocument();
+    expect(
+      getByText('Configure User Access for your Inventory groups')
     ).toBeInTheDocument();
   });
 });
