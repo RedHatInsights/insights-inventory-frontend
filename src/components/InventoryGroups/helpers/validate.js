edecec0517b060ee931c2ee0fcd6456f2f5886fe
Georgii Karataev <georgkarat@gmail.com>
2023-09-06 12:27:58 +0200

    fix(Groups): Update validation warning messages

--- src/components/InventoryGroups/helpers/validate.js
+++ src/components/InventoryGroups/helpers/validate.js
@@ -4,5 +4,5 @@ export const nameValidator = {
   type: validatorTypes.PATTERN,
   pattern: /^[A-Za-z0-9]+[A-Za-z0-9_\-\s]*$/,
   message:
-    'Name must start with alphanumeric characters and can contain underscore and hyphen characters.',
+    'Must start with a letter or number. Valid characters include lowercase letters, numbers, hyphens ( - ), and underscores ( _ ).',
 };
