e7846f3f1645f0c6418b79292ee12374814c92b6
Demetrius Lima <delima@redhat.com>
2023-09-21 19:29:21 -0300

    fix(THEEDGE-3564): Add missing update action button to edge systems

--- config/overrideChrome.js
+++ config/overrideChrome.js
@@ -1,4 +1,4 @@
-export default () => ({
+const chromeMock = {
   updateDocumentTitle: () => undefined,
   isBeta: () => false,
   appAction: () => {},
@@ -7,4 +7,26 @@ export default () => ({
   getApp: () => 'inventory',
   getBundle: () => 'rhel',
   getUserPermissions: () => [{ permission: 'inventory:*:*' }],
-});
+  auth: {
+    getUser: () =>
+      Promise.resolve({
+        identity: {
+          account_number: '0',
+          type: 'User',
+          user: {
+            is_org_admin: true,
+          },
+        },
+        entitlements: {
+          hybrid_cloud: { is_entitled: true },
+          insights: { is_entitled: true },
+          openshift: { is_entitled: true },
+          smart_management: { is_entitled: false },
+        },
+      }),
+  },
+};
+
+export default () => chromeMock;
+
+export const useChrome = () => chromeMock;
