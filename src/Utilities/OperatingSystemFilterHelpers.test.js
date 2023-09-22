953063749f3c4e4e121de19001d844e128123603
Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>
2023-09-22 16:12:42 -0400

    feat(RHINENG-462): Add CentOS version filtering to inv table
    This PR adds operating system filtering for CentOS Linux systems. It should act as it always has, but will now filter on more. Definitely try to break it because the data structure being used for filtering has changed significantly and there are many opportunities for this system to break.

--- src/Utilities/OperatingSystemFilterHelpers.test.js
+++ src/Utilities/OperatingSystemFilterHelpers.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable quote-props */
-import { cloneDeep, set } from 'lodash';
+import { cloneDeep } from 'lodash';
 import {
   buildOSFilterChip,
   compareVersions,
@@ -8,47 +8,15 @@ import {
   onOSFilterChange,
   toGroupSelection,
 } from './OperatingSystemFilterHelpers';
-
-const testValue = {
-  7: {
-    7: true,
-    '7.0': true,
-    7.1: true,
-    7.2: true,
-  },
-  8: {
-    8: true,
-    8.5: true,
-    '8.10': true,
-  },
-  9: {
-    9: true,
-    9.1: true,
-  },
-};
-
-const testVersions = [
-  {
-    label: 'RHEL 6.9',
-    value: '6.9',
-  },
-  {
-    label: 'RHEL 6.10',
-    value: '6.10',
-  },
-  {
-    label: 'RHEL 7.0',
-    value: '7.0',
-  },
-  {
-    label: 'RHEL 8.5',
-    value: '8.5',
-  },
-  {
-    label: 'RHEL 8.10',
-    value: '8.10',
-  },
-];
+import {
+  availableVersions,
+  groupSelection1,
+  groupSelection2,
+  testValue1,
+  testValue2,
+  testValue3,
+  testValue4,
+} from './__mocks__/OperatingSystemFilterHelpers.fixtures';
 
 describe('toGroupSelection', () => {
   it('returns a select value for an empty group selection', () => {
@@ -56,30 +24,13 @@ describe('toGroupSelection', () => {
   });
 
   it('returns a select value for a group selection', () => {
-    expect(
-      toGroupSelection(['7.0', '7.1', '7.2', '8.5', '8.10', '9.1'])
-    ).toEqual(testValue);
+    expect(toGroupSelection(groupSelection1)).toEqual(testValue1);
   });
 
   it('return a select value for a group selection with available version', () => {
-    expect(
-      toGroupSelection(
-        ['7.0', '7.1', '7.2', '8.5', '8.10', '9.1'],
-        ['7.1', '7.2', '7.3', '8.5', '8.10']
-      )
-    ).toEqual({
-      7: {
-        7: false,
-        7.1: true,
-        7.2: true,
-        7.3: false,
-      },
-      8: {
-        8: true,
-        8.5: true,
-        '8.10': true,
-      },
-    });
+    expect(toGroupSelection(groupSelection2, availableVersions)).toEqual(
+      testValue2
+    );
   });
 });
 
@@ -99,17 +50,12 @@ describe('compareVersions', () => {
 
 describe('getSelectedOsFilterVersions', () => {
   it('returns only enabled filter values', () => {
-    const value = cloneDeep(testValue);
-    value['8']['8.5'] = false;
+    const value = cloneDeep(testValue2);
     expect(getSelectedOsFilterVersions({})).toEqual([]);
-    expect(getSelectedOsFilterVersions({ 7: { 7.1: false } })).toEqual([]);
-    expect(getSelectedOsFilterVersions(value)).toEqual([
-      '7.0',
-      '7.1',
-      '7.2',
-      '8.10',
-      '9.1',
-    ]);
+    expect(getSelectedOsFilterVersions({ 'RHEL 7': { 7.1: false } })).toEqual(
+      []
+    );
+    expect(getSelectedOsFilterVersions(value)).toEqual(groupSelection2);
   });
 });
 
@@ -118,82 +64,82 @@ describe('groupOSFilterVersions', () => {
     expect(groupOSFilterVersions([])).toEqual([]);
   });
 
-  it('returns group configuration with 3 groups and 4 values', () => {
-    const config = groupOSFilterVersions(testVersions);
+  it('returns group configuration with 5 groups', () => {
+    const config = groupOSFilterVersions(availableVersions);
     expect(config).toMatchSnapshot();
     expect(config[0].groupSelectable).toEqual(true);
     expect(config[0].label).toEqual('RHEL 8');
-    expect(config[0].value).toEqual('8');
+    expect(config[0].value).toEqual('RHEL 8');
   });
 });
 
 describe('buildOSFilterChip', () => {
-  it('returns two version chips', () => {
-    const chips = buildOSFilterChip(testValue, testVersions);
+  it('returns five version chips', () => {
+    const chips = buildOSFilterChip(testValue2, availableVersions);
     expect(chips).toMatchSnapshot();
-    expect(chips[0].chips).toHaveLength(3);
+    expect(chips[0].chips).toHaveLength(5);
   });
 });
 
 describe('onOSFilterChange', () => {
   const clickedGroup = {
-    value: '8',
+    value: 'RHEL 8',
   };
   const clickedItem = {
-    value: '8.5',
+    value: '8.10',
   };
 
   it('returns new selection with group selection enabled', () => {
-    const selection = cloneDeep(testValue);
-    set(selection, [8, 8], false);
+    const selection = cloneDeep(testValue3);
+    selection['RHEL 8'] = {
+      // eslint-disable-next-line prettier/prettier
+      '8.5': true,
+      '8.10': true,
+    };
     expect(
       onOSFilterChange(undefined, selection, clickedGroup, clickedItem)
-    ).toEqual(testValue);
+    ).toEqual(testValue4);
   });
 
   it('returns new selection with group selection disabled', () => {
-    const selection = cloneDeep(testValue);
-    selection[8][8.1] = false;
-    const expected = cloneDeep(selection);
-    expected[8][8] = false;
+    const selection = cloneDeep(testValue4);
+    selection['RHEL 8'] = {
+      // eslint-disable-next-line prettier/prettier
+      '8.5': false,
+      '8.10': false,
+    };
     expect(
       onOSFilterChange(undefined, selection, clickedGroup, clickedItem)
-    ).toEqual(expected);
+    ).toEqual(testValue3);
   });
 
   it('returns new selection with all minors selected after click on group', () => {
     const clickedItem = {
-      value: '8',
+      value: 'RHEL 8',
     };
-    const selection = cloneDeep(testValue);
-    selection[8] = {
-      8: true,
+    const selection = cloneDeep(testValue3);
+    selection['RHEL 8'] = {
+      'RHEL 8': true,
       8.5: false,
       '8.10': false,
     };
     expect(
       onOSFilterChange(undefined, selection, clickedGroup, clickedItem)
-    ).toEqual(testValue);
+    ).toEqual(testValue4);
   });
 
   it('returns new selection with all minors disabled after click on group', () => {
     const clickedItem = {
-      value: '8',
+      value: 'RHEL 8',
     };
-    const selection = cloneDeep(testValue);
-    selection[8] = {
-      8: false,
+    const selection = cloneDeep(testValue4);
+    selection['RHEL 8'] = {
+      'RHEL 8': false,
       8.5: true,
       '8.10': true,
     };
-    const expected = cloneDeep(testValue);
-    expected[8] = {
-      8: false,
-      8.5: false,
-      '8.10': false,
-    };
     expect(
       onOSFilterChange(undefined, selection, clickedGroup, clickedItem)
-    ).toEqual(expected);
+    ).toEqual(testValue3);
   });
 });
