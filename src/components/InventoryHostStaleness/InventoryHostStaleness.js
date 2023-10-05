45424476320eb9d5281a061e58ab80f929013cfa
Adonis Puente <adonis.puente@gmail.com>
2023-10-05 11:18:47 -0400

    fix(RHINENG-1490): Update RBAC to match new mocks

--- src/components/InventoryHostStaleness/InventoryHostStaleness.js
+++ src/components/InventoryHostStaleness/InventoryHostStaleness.js
@@ -2,9 +2,21 @@
 import React from 'react';
 import HostStalenessCard from './HostStalenessCard';
 import { usePermissionsWithContext } from '@redhat-cloud-services/frontend-components-utilities/RBACHook';
-import { GENERAL_HOST_STALENESS_WRITE_PERMISSION } from './constants';
+import {
+  GENERAL_HOST_STALENESS_READ_PERMISSION,
+  GENERAL_HOST_STALENESS_WRITE_PERMISSION,
+} from './constants';
+import {
+  GENERAL_HOSTS_READ_PERMISSIONS,
+  GENERAL_HOSTS_WRITE_PERMISSIONS,
+} from '../../constants';
 
-const REQUIRED_PERMISSIONS = [GENERAL_HOST_STALENESS_WRITE_PERMISSION];
+const REQUIRED_PERMISSIONS = [
+  GENERAL_HOST_STALENESS_WRITE_PERMISSION,
+  GENERAL_HOST_STALENESS_READ_PERMISSION,
+  GENERAL_HOSTS_READ_PERMISSIONS,
+  GENERAL_HOSTS_WRITE_PERMISSIONS,
+];
 
 const InventoryHostStaleness = () => {
   const { hasAccess: canModifyHostStaleness } =
