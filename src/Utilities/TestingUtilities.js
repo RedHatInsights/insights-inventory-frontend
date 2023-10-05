1bd0b2e0c459066f3454004d9618640db6fd4669
Georgii Karataev <georgkarat@gmail.com>
2023-10-05 15:47:49 +0200

    chore: Enhance TestWrapper and include redux provider

--- src/Utilities/TestingUtilities.js
+++ src/Utilities/TestingUtilities.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
 
 export const mountWithRouter = (Component, initialEntries) => {
   const wrapper = mount(
@@ -10,26 +11,32 @@ export const mountWithRouter = (Component, initialEntries) => {
   return wrapper;
 };
 
-export const RouterWrapper = ({
+export const TestWrapper = ({
   children,
   routerProps = { initialEntries: ['/'] },
   path,
-}) => (
-  <MemoryRouter {...routerProps}>
-    {path ? (
-      <Routes>
-        <Route path={path} element={children} />
-      </Routes>
-    ) : (
-      children
-    )}
-  </MemoryRouter>
-);
+  store = {},
+}) => {
+  return (
+    <MemoryRouter {...routerProps}>
+      <Provider store={store}>
+        {path ? (
+          <Routes>
+            <Route path={path} element={children} />
+          </Routes>
+        ) : (
+          children
+        )}
+      </Provider>
+    </MemoryRouter>
+  );
+};
 
-RouterWrapper.propTypes = {
+TestWrapper.propTypes = {
   children: PropTypes.any.isRequired,
   routerProps: PropTypes.shape({
     initialEntries: PropTypes.arrayOf(PropTypes.string),
   }),
   path: PropTypes.string,
+  store: PropTypes.any,
 };
