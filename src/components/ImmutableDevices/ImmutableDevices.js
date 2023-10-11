5bad25a9bc78f34cf36e5e899f579f90bf2e3b09
Muslimjon <59481011+mkholjuraev@users.noreply.github.com>
2023-10-11 10:52:52 +0200

    feat(RHIF-279): configurable, shared ImmutableDevices table (#2037)
    * feat(RHIF-279: configurable, shared ImmutableDevices table
    
    * chore: expose ImmutableDevice component through fed-mod
    
    * chore(RHIF-277): separate the tab content
    
    * chore: hide checkboxes
    
    * chore(RHIF-279): add correct links in the table, filter config props
    
    * chore(RHIF-279): refactor getStatus func and edgeColumn const

---
+++ src/components/ImmutableDevices/ImmutableDevices.js
@@ -0,0 +1,90 @@
+import propTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { TableVariant } from '@patternfly/react-table';
+import { InventoryTable } from '@redhat-cloud-services/frontend-components/Inventory';
+import useFeatureFlag from '../../Utilities/useFeatureFlag';
+import { useNavigate } from 'react-router-dom';
+import { edgeColumns } from './helpers';
+
+const ImmutableDevices = ({
+  inventoryRef,
+  customFilters,
+  onLoad,
+  getEntities,
+  mergeAppColumns,
+  filterConfig,
+  hideFilters,
+  activeFiltersConfig,
+}) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const inventoryGroupsEnabled = useFeatureFlag('hbi.ui.inventory-groups');
+
+  const totalItems = useSelector(({ entities }) => entities?.total);
+
+  useEffect(() => {
+    return () => {
+      dispatch({
+        type: 'CLEAR_INVENTORY_STORE',
+        payload: [],
+      });
+    };
+  }, [dispatch]);
+
+  const mergeColumns = (inventoryColumns) => {
+    const filteredColumns = inventoryColumns.filter(
+      (column) => !column.inventoryGroupsFeatureFlag || inventoryGroupsEnabled
+    );
+
+    return [...mergeAppColumns(filteredColumns), ...edgeColumns];
+  };
+
+  const onRowClick = (_key, systemId) => {
+    navigate(`/insights/inventory/${systemId}?appName=vulnerabilities`);
+  };
+
+  return (
+    <InventoryTable
+      disableDefaultColumns
+      onLoad={onLoad}
+      hideFilters={hideFilters}
+      tableProps={{
+        isStickyHeader: true,
+        variant: TableVariant.compact,
+      }}
+      paginationProps={{
+        isDisabled: !totalItems,
+      }}
+      showTagModal
+      hasCheckbox={false}
+      isFullView
+      ref={inventoryRef}
+      autoRefresh
+      key="inventory"
+      customFilters={customFilters}
+      columns={(defaultColumns) => mergeColumns(defaultColumns)}
+      getEntities={getEntities}
+      filterConfig={filterConfig}
+      activeFiltersConfig={activeFiltersConfig}
+      onRowClick={onRowClick}
+    />
+  );
+};
+
+ImmutableDevices.propTypes = {
+  inventoryRef: propTypes.oneOfType([
+    propTypes.func,
+    propTypes.shape({ current: propTypes.any }),
+  ]),
+  columns: propTypes.array,
+  customFilters: propTypes.object,
+  onLoad: propTypes.func,
+  getEntities: propTypes.func,
+  mergeAppColumns: propTypes.func,
+  filterConfig: propTypes.object,
+  hideFilters: propTypes.object,
+  activeFiltersConfig: propTypes.object,
+};
+
+export default ImmutableDevices;
