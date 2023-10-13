0887d31f13419c96400ffc5fa5e7d23c97f1666c
Muslimjon Kholjuraev <mkholjur@redhat.com>
2023-10-13 10:00:42 +0200

    chore(prodConfig): enable beta/stable difference for deployments

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -5,6 +5,7 @@ const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   debug: true,
+  ...(process.env.BETA === 'true' && { deployment: 'beta/apps' }),
 });
 
 plugins.push(
