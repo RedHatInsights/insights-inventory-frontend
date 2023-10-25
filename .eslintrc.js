a83c10253f42462260b5d853d5eefae9020a2b15
Georgii Karataev <georgkarat@gmail.com>
2023-10-25 18:02:10 +0200

    chore: Support RTL and install dependencies
    This enables RTL support in the repository so that it is
    possible to write new unit tests using RTL.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: [
     '@redhat-cloud-services/eslint-config-redhat-cloud-services',
     'plugin:cypress/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
   ],
   globals: {
     insights: 'readonly',
