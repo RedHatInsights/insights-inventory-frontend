94b3e3d1030165292e438569ca54ac319f29a600
acosferreia <acosferreira@gmail.com>
2023-11-01 22:38:31 -0700

    feat(THEEDGE-3581): add deviceGroupView

---
+++ src/components/InventoryTabs/ImmutableDevices/EdgeDevicesGroupView.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import AsyncComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
+import ErrorState from '@redhat-cloud-services/frontend-components/ErrorState';
+import { resolveRelPath } from '../../../Utilities/path';
+import {
+  getNotificationProp,
+  manageEdgeInventoryUrlName,
+} from '../../../Utilities/edge';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+const EdgeDeviceGroupiew = (props) => {
+  console.log(props);
+  const dispatch = useDispatch();
+  const notificationProp = getNotificationProp(dispatch);
+  return (
+    <AsyncComponent
+      appName="edge"
+      module="./DevicesGroupDetail"
+      ErrorComponent={<ErrorState />}
+      navigateProp={useNavigate}
+      locationProp={useLocation}
+      showHeaderProp={false}
+      pathPrefix={resolveRelPath('')}
+      urlName={manageEdgeInventoryUrlName}
+      notificationProp={notificationProp}
+      {...props}
+    />
+  );
+};
+
+export default EdgeDeviceGroupiew;
