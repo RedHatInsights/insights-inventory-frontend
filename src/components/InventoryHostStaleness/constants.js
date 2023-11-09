907c4c1687f00030f1d9d9d75c63724eebf28e6e
Adonis Puente <adonis.puente@gmail.com>
2023-11-09 10:39:11 -0500

    feat(ESSNTL-5538): Update RBAC roles for staleness

--- src/components/InventoryHostStaleness/constants.js
+++ src/components/InventoryHostStaleness/constants.js
@@ -10,9 +10,9 @@ export const IMMUTABLE_TAB_TOOLTIP =
   'With OSTree, you can manage the system software by referencing a central image repository. OSTree images contain a complete operating system ready to be remotely installed at scale.  You can track updates to images through commits and enable secure updates that only address changes and keep the operating system unchanged. The updates are quick, and the rollbacks are easy.';
 
 export const GENERAL_HOST_STALENESS_WRITE_PERMISSION =
-  'inventory:staleness:write';
+  'staleness:staleness:write';
 export const GENERAL_HOST_STALENESS_READ_PERMISSION =
-  'inventory:staleness:read';
+  'staleness:staleness:read';
 
 export const HOST_STALENESS_ADMINISTRATOR_PERMISSIONS = [
   GENERAL_HOST_STALENESS_READ_PERMISSION,
