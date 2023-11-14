4e3a0299e907dd23a1579cdfa639a4d493ea1dcd
mgold1234 <mgold@redhat.com>
2023-11-14 14:29:17 +0200

    fix(InventoryGroupDetails): THEEDGE-3707 implement edge group details expostion

---
+++ src/routes/InventoryOrEdgeGroupDetailsComponent.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { inventoryHasConventionalSystems } from '../Utilities/conventional';
+import { inventoryHasEdgeSystems } from '../Utilities/edge';
+import useEdgeGroups from '../Utilities/hooks/useEdgeGroups';
+import useFeatureFlag from '../Utilities/useFeatureFlag';
+import PropTypes from 'prop-types';
+import { Spinner } from '@patternfly/react-core';
+import EdgeGroupsDetailsView from '../components/InventoryGroupDetail/EdgeGroupDetails';
+import InventoryGroupDetail from './InventoryGroupDetail';
+
+const InventoryOrEdgeGroupDetailsView = () => {
+  const [enforceEdgeGroups, isLoading] = useEdgeGroups();
+  const [hasConventionalSystems, setHasConventionalSystems] = useState(true);
+  const [hasEdgeDevices, setHasEdgeDevices] = useState(true);
+  const edgeParityInventoryListEnabled = useFeatureFlag(
+    'edgeParity.inventory-list'
+  );
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const hasConventionalSystems = await inventoryHasConventionalSystems();
+        if (edgeParityInventoryListEnabled) {
+          const hasEdgeSystems = await inventoryHasEdgeSystems();
+          setHasConventionalSystems(hasConventionalSystems);
+          setHasEdgeDevices(hasEdgeSystems);
+        }
+      })();
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  const GroupsDetailComponents = enforceEdgeGroups
+    ? EdgeGroupsDetailsView
+    : InventoryGroupDetail;
+  if (!isLoading) {
+    return (
+      <GroupsDetailComponents
+        hasConventionalSystems={hasConventionalSystems}
+        hasEdgeDevices={hasEdgeDevices}
+      />
+    );
+  } else {
+    return <Spinner />;
+  }
+};
+
+InventoryOrEdgeGroupDetailsView.prototype = {
+  enforceEdgeGroups: PropTypes.bool,
+  isLoading: PropTypes.bool,
+};
+
+export default InventoryOrEdgeGroupDetailsView;
