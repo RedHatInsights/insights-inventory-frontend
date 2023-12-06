fce924cde70dc7ec1841368b9f685f2610d279de
Georgii Karataev <georgkarat@gmail.com>
2023-12-06 12:40:23 +0100

    fix(Create group): Ignore empty input
    This makes the create group modal not to validate group name presence if
    the input is empty. It previously led to the modal crash sometimes if
    the input field was empty.

--- src/components/InventoryGroups/Modals/CreateGroupModal.test.js
+++ src/components/InventoryGroups/Modals/CreateGroupModal.test.js
@@ -4,22 +4,41 @@ import { validate } from './CreateGroupModal';
 jest.mock('../utils/api');
 
 describe('validate function', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('works with basic input', async () => {
     const result = await validate('test');
 
-    expect(result).toBe(undefined);
+    expect(result).toBeUndefined();
     expect(validateGroupName).toHaveBeenCalledWith('test');
   });
 
   it('trims input', async () => {
     const result = await validate(' test ');
 
-    expect(result).toBe(undefined);
+    expect(result).toBeUndefined();
     expect(validateGroupName).toHaveBeenCalledWith('test');
   });
 
   it('throws error if the name is present', async () => {
     validateGroupName.mockResolvedValue(true);
+
     await expect(validate('test')).rejects.toBe('Group name already exists');
+  });
+
+  it('does not check on undefined input', async () => {
+    const result = await validate(undefined);
+
+    expect(result).toBeUndefined();
+    expect(validateGroupName).not.toHaveBeenCalled();
+  });
+
+  it('does not check on empty input', async () => {
+    const result = await validate('');
+
+    expect(result).toBeUndefined();
+    expect(validateGroupName).not.toHaveBeenCalled();
   });
 });
