92f19f1e9088eed7061a795c8caaae3813c6e986
acosferreia <acosferreira@gmail.com>
2023-12-11 21:19:15 -0800

    fix(THEEDGE-3739): remove checkbox on header

--- src/components/GroupSystems/GroupImmutableSystems.js
+++ src/components/GroupSystems/GroupImmutableSystems.js
@@ -251,7 +251,7 @@ const GroupImmutableSystems = ({ groupName, groupId, ...props }) => {
           tableProps={{
             isStickyHeader: true,
             variant: TableVariant.compact,
-            canSelectAll: true,
+            canSelectAll: false,
             actionResolver: (row) => [
               {
                 title: (
