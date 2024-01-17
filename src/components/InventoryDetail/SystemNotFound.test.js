9649731b7638cebb8c78ce35bf5f76073caa9419
Georgii Karataev <georgkarat@gmail.com>
2024-01-17 13:40:13 +0100

    chore(SystemNotFound): Migrate test to RTL

--- src/components/InventoryDetail/SystemNotFound.test.js
+++ src/components/InventoryDetail/SystemNotFound.test.js
@@ -1,83 +1,42 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import toJson from 'enzyme-to-json';
+import { renderWithRouter } from '../../Utilities/TestingUtilities';
 import SystemNotFound from './SystemNotFound';
-import { mountWithRouter } from '../../Utilities/TestingUtilities';
-//import useInsightsNavigate from '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate/useInsightsNavigate';
 
-jest.mock(
-  '@redhat-cloud-services/frontend-components-utilities/useInsightsNavigate/useInsightsNavigate',
-  () => () => jest.fn()
-);
-describe('EntityTable', () => {
-  describe('DOM', () => {
-    it('should render correctly', () => {
-      const wrapper = mountWithRouter(<SystemNotFound />);
-      expect(toJson(wrapper)).toMatchSnapshot();
+describe('SystemNotFound', () => {
+  it('should render correctly', () => {
+    renderWithRouter(<SystemNotFound />);
+
+    screen.getByRole('heading', {
+      name: /system not found/i,
     });
-
-    it('should render correctly with inv ID', () => {
-      const wrapper = mountWithRouter(
-        <SystemNotFound inventoryId="something" />
-      );
-      expect(toJson(wrapper)).toMatchSnapshot();
+    screen.getByText(/system with id does not exist/i);
+    screen.getByRole('button', {
+      name: /back to previous page/i,
     });
   });
 
-  describe('API', () => {
-    const replace = jest.fn();
-    const back = jest.fn();
-    beforeEach(() => {
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: {
-          pathname: {
-            replace,
-          },
-          href: '',
-        },
-      });
+  it('should render correctly with inv ID', () => {
+    renderWithRouter(<SystemNotFound inventoryId="something" />);
 
-      Object.defineProperty(window, 'history', {
-        writable: true,
-        value: {
-          back,
-        },
-      });
-    });
-
-    it('should call location replace correctly', () => {
-      const onBackToListClick = jest.fn();
-      const wrapper = mountWithRouter(
-        <SystemNotFound inventoryId="something" />
-      );
-      wrapper.find('button').first().simulate('click');
-      expect(onBackToListClick).not.toHaveBeenCalled();
-      expect(replace).toHaveBeenCalled();
-    });
-
-    // it('should call history correctly', () => {
-    //   Object.defineProperty(document, 'referrer', {
-    //     writable: true,
-    //     value: true,
-    //   });
-    //   const onBackToListClick = jest.fn();
-    //   const wrapper = mountWithRouter(
-    //     <SystemNotFound inventoryId="something" />
-    //   );
-    //   wrapper.find('button').first().simulate('click');
-    //   expect(onBackToListClick).not.toHaveBeenCalled();
-    // });
-
-    it('should call onBackToListClick correctly', () => {
-      const onBackToListClick = jest.fn();
-      const wrapper = mountWithRouter(
-        <SystemNotFound
-          inventoryId="something"
-          onBackToListClick={onBackToListClick}
-        />
-      );
-      wrapper.find('button').first().simulate('click');
-      expect(onBackToListClick).toHaveBeenCalled();
-    });
+    screen.getByText(/system with id something does not exist/i);
   });
+});
+
+it('should call onBackToListClick correctly', async () => {
+  const onBackToListClick = jest.fn();
+  renderWithRouter(
+    <SystemNotFound
+      inventoryId="something"
+      onBackToListClick={onBackToListClick}
+    />
+  );
+
+  await userEvent.click(
+    screen.getByRole('button', {
+      name: /back to previous page/i,
+    })
+  );
+  expect(onBackToListClick).toHaveBeenCalled();
 });
