0d2b52d2210e3784cfda7b01d58961c5226a7303
Muslimjon <mkholjur@redhat.com>
2024-04-02 12:27:21 +0200

    chore(RHINENG-8881): use redux hooks instead of legacy connect function

--- src/components/GeneralInfo/BootcImageCard/BootcImageCard.js
+++ src/components/GeneralInfo/BootcImageCard/BootcImageCard.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import LoadingCard from '../LoadingCard';
 import { bootcSelector } from '../selectors';
 import { extraShape } from '../../../constants';
 
-const BootcImageCardCore = ({ bootc, detailLoaded, handleClick, extra }) => (
-  <LoadingCard
-    title="BOOTC"
-    isLoading={!detailLoaded}
-    items={[
-      ...[{ title: 'Booted Image', value: bootc.bootedImage }],
-      ...[{ title: 'Booted Image Digest', value: bootc.bootedImageDigest }],
-      ...[{ title: 'Staged Image', value: bootc.stagedImage }],
-      ...[{ title: 'Staged Image Digest', value: bootc.stagedImageDigest }],
-      ...[{ title: 'Rollback Image', value: bootc.rollbackImage }],
-      ...[{ title: 'Rollback Image Digest', value: bootc.rollbackImageDigest }],
-      ...extra.map(({ onClick, ...item }) => ({
-        ...item,
-        ...(onClick && { onClick: (e) => onClick(e, handleClick) }),
-      })),
-    ]}
-  />
-);
+export const BootcImageCard = ({ handleClick, extra }) => {
+  const { detailLoaded, bootc } = useSelector(
+    ({ systemProfileStore: { systemProfile } }) => ({
+      detailLoaded: systemProfile && systemProfile.loaded,
+      bootc: bootcSelector(systemProfile),
+    })
+  );
 
-BootcImageCardCore.propTypes = {
+  return (
+    <LoadingCard
+      title="BOOTC"
+      isLoading={!detailLoaded}
+      items={[
+        ...[{ title: 'Booted Image', value: bootc.bootedImage }],
+        ...[{ title: 'Booted Image Digest', value: bootc.bootedImageDigest }],
+        ...[{ title: 'Staged Image', value: bootc.stagedImage }],
+        ...[{ title: 'Staged Image Digest', value: bootc.stagedImageDigest }],
+        ...[{ title: 'Rollback Image', value: bootc.rollbackImage }],
+        ...[
+          { title: 'Rollback Image Digest', value: bootc.rollbackImageDigest },
+        ],
+        ...extra.map(({ onClick, ...item }) => ({
+          ...item,
+          ...(onClick && { onClick: (e) => onClick(e, handleClick) }),
+        })),
+      ]}
+    />
+  );
+};
+
+BootcImageCard.propTypes = {
   detailLoaded: PropTypes.bool,
   handleClick: PropTypes.func,
   bootc: PropTypes.shape({
@@ -37,20 +48,8 @@ BootcImageCardCore.propTypes = {
   }),
   extra: PropTypes.arrayOf(extraShape),
 };
-BootcImageCardCore.defaultProps = {
+BootcImageCard.defaultProps = {
   detailLoaded: false,
   handleClick: () => undefined,
   extra: [],
 };
-
-export const BootcImageCard = connect(
-  ({ systemProfileStore: { systemProfile } }) => ({
-    detailLoaded: systemProfile && systemProfile.loaded,
-    bootc: bootcSelector(systemProfile),
-  })
-)(BootcImageCardCore);
-
-BootcImageCard.propTypes = BootcImageCardCore.propTypes;
-BootcImageCard.defaultProps = BootcImageCardCore.defaultProps;
-
-export default BootcImageCard;
--- src/components/GeneralInfo/BootcImageCard/index.js
+++ src/components/GeneralInfo/BootcImageCard/index.js
@@ -1,2 +1,1 @@
 export * from './BootcImageCard';
-export { default } from './BootcImageCard';
