0af33748ab145df718129e42b85028ab7b3edcd8
Muslimjon <mkholjur@redhat.com>
2024-04-16 15:01:29 +0200

    chore: fix tests

--- src/routes/InventoryComponents/InventoryPageHeader.test.js
+++ src/routes/InventoryComponents/InventoryPageHeader.test.js
@@ -3,64 +3,79 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import useFeatureFlag from '../../Utilities/useFeatureFlag';
-
+import { AccountStatContext } from '../../Routes';
 import InventoryPageHeader from './InventoryPageHeader';
 
 jest.mock('../../Utilities/useFeatureFlag');
 
+const defaultContextValues = {
+  hasConventionalSystems: true,
+  hasEdgeDevices: true,
+  hasBootcImages: true,
+};
+
+const defaultProps = {
+  changeMainContent: jest.fn(() => () => {}),
+  mainContent: 'bifrost',
+};
+
+const mountWithContext = (
+  props = defaultProps,
+  contextValues = defaultContextValues
+) => {
+  render(
+    <AccountStatContext.Provider value={{ ...contextValues }}>
+      <InventoryPageHeader {...props} />
+    </AccountStatContext.Provider>
+  );
+};
 const user = userEvent.setup();
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 describe('InventoryContentToggle', () => {
   test('renders toggle items with correct labels and icons', () => {
-    const changeMainContentMock = jest.fn();
-    const mainContent = 'bifrost';
     useFeatureFlag.mockReturnValue(true);
 
-    render(
-      <InventoryPageHeader
-        changeMainContent={changeMainContentMock}
-        mainContent={mainContent}
-      />
-    );
-
-    expect(screen.getByLabelText('Hybrid inventory')).toBeInTheDocument();
-    expect(screen.getByLabelText('Bifrost')).toBeInTheDocument();
+    mountWithContext();
+    expect(screen.getByLabelText('Hybrid inventory')).toBeVisible();
+    expect(screen.getByLabelText('Bifrost')).toBeVisible();
     expect(screen.getByLabelText('Bifrost')).toHaveAttribute(
       'aria-pressed',
       'true'
     );
   });
 
-  test('calls changeMainContent with correct content key when Bifrost is clicked', async () => {
-    const changeMainContentMock = jest.fn();
-    const mainContent = 'someContent';
+  test('hides toggle items when there is no bootc images', () => {
+    useFeatureFlag.mockReturnValue(true);
 
-    render(
-      <InventoryPageHeader
-        changeMainContent={changeMainContentMock}
-        mainContent={mainContent}
-      />
-    );
+    mountWithContext(defaultProps, {
+      ...defaultContextValues,
+      hasBootcImages: false,
+    });
+    expect(screen.queryByLabelText('Hybrid inventory')).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('Bifrost')).not.toBeInTheDocument();
+    expect(screen.getByText('Systems')).toBeVisible();
+  });
+
+  test('calls changeMainContent with correct content key when Bifrost is clicked', async () => {
+    mountWithContext();
 
     await user.click(screen.getByLabelText('Bifrost'));
 
-    expect(changeMainContentMock).toHaveBeenCalledWith('bifrost');
-    expect(changeMainContentMock).toHaveBeenCalledTimes(1);
+    expect(defaultProps.changeMainContent).toHaveBeenCalledWith('bifrost');
+    expect(defaultProps.changeMainContent).toHaveBeenCalledTimes(1);
   });
 
   test('calls changeMainContent with correct content key when Hybrid inventory is clicked', async () => {
-    const changeMainContentMock = jest.fn();
-    const mainContent = 'someContent';
-
-    render(
-      <InventoryPageHeader
-        changeMainContent={changeMainContentMock}
-        mainContent={mainContent}
-      />
-    );
+    mountWithContext();
 
     await user.click(screen.getByLabelText('Hybrid inventory'));
 
-    expect(changeMainContentMock).toHaveBeenCalledWith('hybridInventory');
-    expect(changeMainContentMock).toHaveBeenCalledTimes(1);
+    expect(defaultProps.changeMainContent).toHaveBeenCalledWith(
+      'hybridInventory'
+    );
+    expect(defaultProps.changeMainContent).toHaveBeenCalledTimes(1);
   });
 });
