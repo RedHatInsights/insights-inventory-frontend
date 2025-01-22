c76eed871f4db212f998d5f434032037a66e79ad
Sebastian Gräßl <mail@bastilian.me>
2025-01-22 11:46:15 +0100

    chore(deps): Bump & cleanup packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
+  parser: '@babel/eslint-parser',
   extends: [
     '@redhat-cloud-services/eslint-config-redhat-cloud-services',
     'plugin:cypress/recommended',
     'plugin:testing-library/react',
     'plugin:jest-dom/recommended',
+    'plugin:react-hooks/recommended',
   ],
   globals: {
     IS_DEV: 'readonly',
+    React: true,
   },
   rules: {
     'rulesdir/forbid-pf-relative-imports': 'off',
