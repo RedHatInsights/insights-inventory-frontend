1f38ebfc61133ae0e680e6ea285644dfaf5b8fab
Sebastian Gräßl <bastilian@users.noreply.github.com>
2025-01-24 11:31:01 +0100

    feat(InventoryTable): RHINENG-15246 - Reword export dropdown items (#2301)
    * feat(InventoryTable): RHINENG-15246 - Reword export dropdown items
    
    * chore(deps): Bump config-utils to avoid proxy issues
    
    * chore(tests): Update wording in test
    
    * fix(cypress): Ensure chrome is mocked on window

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -1,3 +1,8 @@
 import '@patternfly/patternfly/patternfly.scss';
 import '@cypress/code-coverage/support';
 import './commands';
+
+import chrome from '../../config/overrideChrome';
+
+// eslint-disable-next-line rulesdir/no-chrome-api-call-from-window
+window.insights = { chrome };
