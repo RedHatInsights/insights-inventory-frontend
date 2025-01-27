bca41eaa90dcc5b419847597b0829d6d17a414f7
Michael Johnson <micjohns@micjohns-thinkpadp16vgen1.rmtusnc.csb>
2025-01-27 17:41:34 -0500

    fix(RHINENG-13686): Update last seen filter to single select
    This PR converts the last seen filter on the systems table and workspace
    details table from radio buttons to single select.
    
    To test:
    - go to /insights/inventory and use the last seen filter
    - go to /insights/inventory/workspaces/<id> and use the last seen
      filter

---
+++ src/components/filters/useLastSeenFilter.test.js
@@ -0,0 +1,45 @@
+import { lastSeenFilterItems } from '../../Utilities';
+import { useLastSeenFilter } from './useLastSeenFilter';
+import { renderHook } from '@testing-library/react';
+
+const mockDispatch = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useDispatch: () => mockDispatch,
+}));
+
+describe('useLastSeenFilter', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('triggers navigate on filter', () => {
+    const { result } = renderHook(() =>
+      useLastSeenFilter([
+        {
+          lastSeenFilter: {
+            mark: 'last24',
+            updatedEnd: '2025-01-27T22:06:08.176Z',
+            updatedStart: '2025-01-26T22:06:08.176Z',
+          },
+        },
+        () => jest.fn(),
+      ])
+    );
+
+    const [filter] = result.current;
+    expect(JSON.stringify(filter)).toEqual(
+      JSON.stringify({
+        label: 'Last seen',
+        value: 'last_seen',
+        type: 'singleSelect',
+        filterValues: {
+          value: 'last24',
+          onChange: () => jest.fn(),
+          items: lastSeenFilterItems,
+        },
+      })
+    );
+  });
+});
