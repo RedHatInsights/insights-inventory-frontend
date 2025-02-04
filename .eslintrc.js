6ed7740eea291fc3846cc4b5631fadbf22849107
Georgii Karataev <georgkarat@gmail.com>
2025-02-04 14:03:44 +0100

    chore: Set up JSDoc docs generation script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   parser: '@babel/eslint-parser',
+  plugins: ['jsdoc'],
   extends: [
     '@redhat-cloud-services/eslint-config-redhat-cloud-services',
     'plugin:cypress/recommended',
     'plugin:testing-library/react',
     'plugin:jest-dom/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsdoc/recommended',
   ],
   rules: {
     'rulesdir/forbid-pf-relative-imports': 'off',
