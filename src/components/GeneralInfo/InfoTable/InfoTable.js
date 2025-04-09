160e8a52e5f1cfcc1f9c2beb4f37ecf82571ed8d
Hugo Hezel <hhezel@redhat.com>
2025-04-09 09:55:16 +0200

    feat(RHINENG-17344): Convert class component InfoTable into functional one

--- src/components/GeneralInfo/InfoTable/InfoTable.js
+++ src/components/GeneralInfo/InfoTable/InfoTable.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Pagination,
@@ -15,78 +15,35 @@ import {
   TableHeader,
 } from '@patternfly/react-table/deprecated';
 import flatMap from 'lodash/flatMap';
-import {
-  filterRows,
-  generateFilters,
-  onDeleteFilter,
-  prepareRows,
-} from '../../../constants';
-class InfoTable extends Component {
-  state = {
-    sortBy: { index: 0, direction: SortByDirection.asc },
-    opened: [],
-    pagination: {
-      page: 1,
-      perPage: 10,
-    },
-    activeFilters: {},
-  };
+import { filterRows, generateFilters, prepareRows } from '../../../constants';
 
-  onSort = (event, index, direction) => {
-    const { expandable } = this.props;
-    this.props.onSort(event, expandable ? index - 1 : index, direction);
-    this.setState({
-      sortBy: {
-        index,
-        direction,
-      },
-    });
-  };
+const InfoTable = ({
+  cells = [],
+  rows = [],
+  expandable = false,
+  filters,
+  onSort = undefined,
+}) => {
+  // States
+  const [sortBy, setSortBy] = useState({
+    index: 0,
+    direction: SortByDirection.asc,
+  });
+  const [opened, setOpened] = useState([]);
+  const [pagination, setPagination] = useState({
+    page: 1,
+    perPage: 10,
+  });
+  const [activeFilters, setActiveFilters] = useState({});
 
-  onCollapse = (_event, index, isOpen) => {
-    const { opened } = this.state;
-    opened[index] = isOpen;
-    this.setState({
-      opened,
-    });
-  };
+  // Memo
+  const activeRows = useMemo(
+    () => filterRows(rows, activeFilters),
+    [rows, activeFilters]
+  );
 
-  onUpdatePagination = ({ page, perPage }) => {
-    this.setState({ pagination: { ...this.state.pagination, page, perPage } });
-  };
-
-  setFilter = (key, value, label) => {
-    const { activeFilters } = this.state;
-    // eslint-disable-next-line no-unused-vars
-    const { [key]: currFilter, ...restFilter } = activeFilters;
-    this.setState({
-      activeFilters: {
-        ...restFilter,
-        ...(value.length !== 0 && {
-          [key]: { key, value, label },
-        }),
-      },
-      pagination: { ...this.state.pagination, page: 1 },
-    });
-  };
-
-  onDeleteFilter = (_e, [deleted], deleteAll) => {
-    this.setState({
-      activeFilters: onDeleteFilter(
-        deleted,
-        deleteAll,
-        this.state.activeFilters
-      ),
-      pagination: { ...this.state.pagination, page: 1 },
-    });
-  };
-
-  render() {
-    const { cells, rows, expandable, filters } = this.props;
-    const { sortBy, opened, pagination, activeFilters } = this.state;
-    const collapsibleProps = expandable ? { onCollapse: this.onCollapse } : {};
-    const activeRows = filterRows(rows, activeFilters);
-    const mappedRows = expandable
+  const mappedRows = useMemo(() => {
+    return expandable
       ? flatMap(
           prepareRows(activeRows, pagination),
           ({ child, ...row }, key) => [
@@ -101,86 +58,131 @@ class InfoTable extends Component {
           ]
         )
       : prepareRows(activeRows, pagination);
-    return (
-      <Fragment>
-        <PrimaryToolbar
-          pagination={{
-            ...pagination,
-            itemCount: activeRows.length,
-            onSetPage: (_e, page) =>
-              this.onUpdatePagination({ ...pagination, page }),
-            onPerPageSelect: (_e, perPage) =>
-              this.onUpdatePagination({ ...pagination, page: 1, perPage }),
-            titles: {
-              optionsToggleAriaLabel: 'Items per page',
-            },
+  }, [activeRows, pagination, expandable, opened]);
+
+  // Handlers
+  const handleSort = useCallback(
+    (event, index, direction) => {
+      if (onSort) {
+        onSort(event, expandable ? index - 1 : index, direction);
+      }
+      setSortBy({
+        sortBy: {
+          index,
+          direction,
+        },
+      });
+    },
+    [expandable, onSort]
+  );
+
+  const onCollapse = (_event, index, isOpen) => {
+    setOpened((prevOpened) => {
+      const newOpened = [...prevOpened];
+      opened[index] = isOpen;
+      return newOpened;
+    });
+  };
+
+  const onUpdatePagination = ({ page, perPage }) => {
+    setPagination({ page, perPage });
+  };
+
+  const setFilter = (key, value, label) => {
+    setActiveFilters((prevFilters) => {
+      const { [key]: currFilter, ...restFilter } = prevFilters;
+      return {
+        ...restFilter,
+        ...(value.length !== 0 && {
+          [key]: { key, value, label },
+        }),
+      };
+    });
+    setPagination((prevPagination) => ({ ...prevPagination, page: 1 }));
+  };
+
+  const onDeleteFilter = (_e, [deleted], deleteAll) => {
+    setActiveFilters((prevFilters) => {
+      if (deleteAll) {
+        return {};
+      }
+      const { [deleted.key]: _, ...restFilters } = prevFilters;
+      return restFilters;
+    });
+    setPagination((prevPagination) => ({ ...prevPagination, page: 1 }));
+  };
+
+  return (
+    <Fragment>
+      <PrimaryToolbar
+        pagination={{
+          ...pagination,
+          itemCount: activeRows.length || 0,
+          onSetPage: (_e, page) => onUpdatePagination({ ...pagination, page }),
+          onPerPageSelect: (_e, perPage) =>
+            onUpdatePagination({ ...pagination, page: 1, perPage }),
+          titles: {
+            optionsToggleAriaLabel: 'Items per page',
+          },
+        }}
+        {...(filters && {
+          filterConfig: {
+            items: generateFilters(cells, filters, activeFilters, setFilter),
+          },
+        })}
+        activeFiltersConfig={{
+          filters: Object.values(activeFilters).map((filter) => ({
+            ...filter,
+            category: filter.label,
+            chips: Array.isArray(filter.value)
+              ? filter.value.map((item) => ({ name: item }))
+              : [{ name: filter.value || '' }],
+          })),
+          onDelete: onDeleteFilter,
+        }}
+      />
+      {cells.length !== 1 ? (
+        <Table
+          aria-label="General information dialog table"
+          variant={TableVariant.compact}
+          cells={cells}
+          rows={mappedRows}
+          sortBy={{
+            ...sortBy,
+            index: expandable && sortBy.index === 0 ? 1 : sortBy.index,
           }}
-          {...(filters && {
-            filterConfig: {
-              items: generateFilters(
-                cells,
-                filters,
-                activeFilters,
-                this.setFilter
-              ),
-            },
-          })}
-          activeFiltersConfig={{
-            filters: Object.values(activeFilters).map((filter) => ({
-              ...filter,
-              category: filter.label,
-              chips: Array.isArray(filter.value)
-                ? filter.value.map((item) => ({ name: item }))
-                : [{ name: filter.value }],
-            })),
-            onDelete: this.onDeleteFilter,
+          onSort={handleSort}
+          {...(expandable && { onCollapse })}
+        >
+          <TableHeader />
+          <TableBody />
+        </Table>
+      ) : (
+        <TextContent>
+          {prepareRows(activeRows, pagination).map((row, key) => (
+            <Text component={TextVariants.p} key={key}>
+              {row.title || row}
+            </Text>
+          ))}
+        </TextContent>
+      )}
+      <TableToolbar isFooter className="ins-c-inventory__table--toolbar">
+        <Pagination
+          {...pagination}
+          itemCount={activeRows.length || 0}
+          variant="bottom"
+          onSetPage={(_e, page) => onUpdatePagination({ ...pagination, page })}
+          onPerPageSelect={(_e, perPage) =>
+            onUpdatePagination({ ...pagination, page: 1, perPage })
+          }
+          titles={{
+            optionsToggleAriaLabel: 'Items per page',
           }}
         />
-        {cells.length !== 1 ? (
-          <Table
-            aria-label="General information dialog table"
-            variant={TableVariant.compact}
-            cells={cells}
-            rows={mappedRows}
-            sortBy={{
-              ...sortBy,
-              index: expandable && sortBy.index === 0 ? 1 : sortBy.index,
-            }}
-            onSort={this.onSort}
-            {...collapsibleProps}
-          >
-            <TableHeader />
-            <TableBody />
-          </Table>
-        ) : (
-          <TextContent>
-            {prepareRows(activeRows, pagination).map((row, key) => (
-              <Text component={TextVariants.p} key={key}>
-                {row.title || row}
-              </Text>
-            ))}
-          </TextContent>
-        )}
-        <TableToolbar isFooter className="ins-c-inventory__table--toolbar">
-          <Pagination
-            {...pagination}
-            itemCount={activeRows.length}
-            variant="bottom"
-            onSetPage={(_e, page) =>
-              this.onUpdatePagination({ ...pagination, page })
-            }
-            onPerPageSelect={(_e, perPage) =>
-              this.onUpdatePagination({ ...pagination, page: 1, perPage })
-            }
-            titles={{
-              optionsToggleAriaLabel: 'Items per page',
-            }}
-          />
-        </TableToolbar>
-      </Fragment>
-    );
-  }
-}
+      </TableToolbar>
+    </Fragment>
+  );
+};
 
 InfoTable.propTypes = {
   rows: PropTypes.array,
@@ -200,13 +202,6 @@ InfoTable.propTypes = {
       ),
     })
   ),
-};
-InfoTable.defaultProps = {
-  cells: [],
-  rows: [],
-  onSort: () => undefined,
-  sortBy: {},
-  expandable: false,
 };
 
 export default InfoTable;
