40bb34c014f8db57f5c7e74cc5ce23a870595566
Hugo Hezel <hhezel@redhat.com>
2025-04-14 10:09:24 +0200

    feat(RHINENG-17383): Convert class component SystemCardCore into functional one

--- src/components/GeneralInfo/SystemCard/SystemCard.js
+++ src/components/GeneralInfo/SystemCard/SystemCard.js
@@ -1,6 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import LoadingCard from '../LoadingCard';
 import { propertiesSelector } from '../selectors';
 import { editAnsibleHost, editDisplayName } from '../../../store/actions';
@@ -10,270 +10,226 @@ import EditButton from '../EditButton';
 import { generalMapper } from '../dataMapper';
 import { extraShape } from '../../../constants';
 
-class SystemCardCore extends Component {
-  state = {
-    isDisplayNameModalOpen: false,
-    isAnsibleHostModalOpen: false,
+const SystemCard = ({
+  setDisplayName,
+  setAnsibleHost,
+  writePermissions,
+  handleClick,
+  hasHostName = true,
+  hasDisplayName = true,
+  hasAnsibleHostname = true,
+  hasWorkspace = true,
+  hasSAP = true,
+  hasSystemPurpose = true,
+  hasCPUs = true,
+  hasSockets = true,
+  hasCores = true,
+  hasCPUFlags = true,
+  hasRAM = true,
+  extra = [],
+}) => {
+  const dispatch = useDispatch();
+  const entity = useSelector((state) => state.entityDetails.entity);
+  const systemProfile = useSelector(
+    (state) => state.systemProfileStore.systemProfile
+  );
+  const detailLoaded = systemProfile && systemProfile.loaded;
+  const properties = propertiesSelector(systemProfile, entity);
+
+  const [isDisplayNameModalOpen, setIsDisplayNameModalOpen] = useState(false);
+  const [isAnsibleHostModalOpen, setIsAnsibleHostModalOpen] = useState(false);
+
+  const onCancel = () => {
+    setIsDisplayNameModalOpen(false);
+    setIsAnsibleHostModalOpen(false);
   };
 
-  onCancel = () => {
-    this.setState({
-      isDisplayNameModalOpen: false,
-      isAnsibleHostModalOpen: false,
-    });
+  const onSubmit = (fn, origValue) => (value) => {
+    dispatch(fn(entity.id, value, origValue));
+    onCancel();
   };
 
-  onSubmit = (fn, origValue) => (value) => {
-    const { entity } = this.props;
-    fn(entity.id, value, origValue);
-    this.onCancel();
-  };
-
-  onShowDisplayModal = (event) => {
+  const onShowDisplayModal = (event) => {
     event.preventDefault();
-    this.setState({
-      isDisplayNameModalOpen: true,
-    });
+    setIsDisplayNameModalOpen(true);
   };
 
-  onShowAnsibleModal = (event) => {
+  const onShowAnsibleModal = (event) => {
     event.preventDefault();
-    this.setState({
-      isAnsibleHostModalOpen: true,
-    });
+    setIsAnsibleHostModalOpen(true);
   };
 
-  getAnsibleHost = () => {
-    const { entity } = this.props;
+  const getAnsibleHost = () => {
     return entity.ansible_host || entity.fqdn || entity.id;
   };
 
-  render() {
-    const {
-      detailLoaded,
-      entity,
-      properties,
-      setDisplayName,
-      setAnsibleHost,
-      writePermissions,
-      handleClick,
-      hasHostName,
-      hasDisplayName,
-      hasAnsibleHostname,
-      hasWorkspace,
-      hasSAP,
-      hasSystemPurpose,
-      hasCPUs,
-      hasSockets,
-      hasCores,
-      hasCPUFlags,
-      hasRAM,
-      extra,
-    } = this.props;
-    const { isDisplayNameModalOpen, isAnsibleHostModalOpen } = this.state;
+  return (
+    <Fragment>
+      <LoadingCard
+        title="System properties"
+        isLoading={!detailLoaded}
+        cardId="system-card"
+        items={[
+          ...(hasHostName
+            ? [
+                {
+                  title: (
+                    <TitleWithPopover
+                      title="Host name"
+                      content="Name imported from the system."
+                    />
+                  ),
+                  value: entity.fqdn,
+                  size: 'md',
+                  customClass: 'sentry-mask data-hj-suppress',
+                },
+              ]
+            : []),
+          ...(hasDisplayName
+            ? [
+                {
+                  title: (
+                    <TitleWithPopover
+                      title="Display name"
+                      content="System name displayed in an inventory list."
+                    />
+                  ),
+                  value: (
+                    <Fragment>
+                      {entity.display_name}
+                      <EditButton
+                        writePermissions={writePermissions}
+                        link="display_name"
+                        onClick={onShowDisplayModal}
+                      />
+                    </Fragment>
+                  ),
+                  size: 'md',
+                  customClass: 'sentry-mask data-hj-suppress',
+                },
+              ]
+            : []),
+          ...(hasAnsibleHostname
+            ? [
+                {
+                  title: (
+                    <TitleWithPopover
+                      title="Ansible hostname"
+                      content="Hostname that is used in playbooks by Remediations."
+                    />
+                  ),
+                  value: (
+                    <Fragment>
+                      {getAnsibleHost()}
+                      <EditButton
+                        writePermissions={writePermissions}
+                        link="ansible_name"
+                        onClick={onShowAnsibleModal}
+                      />
+                    </Fragment>
+                  ),
+                  size: 'md',
+                  customClass: 'sentry-mask data-hj-suppress',
+                },
+              ]
+            : []),
+          ...(hasWorkspace
+            ? [
+                {
+                  title: 'Workspace',
+                  value: entity.groups?.length > 0 && entity.groups?.[0]?.name,
+                  size: 'md',
+                  customClass: 'sentry-mask data-hj-suppress',
+                  target: `/workspaces/${entity.groups?.[0]?.id}`,
+                },
+              ]
+            : []),
+          ...(hasSAP
+            ? [
+                {
+                  title: 'SAP',
+                  value: properties.sapIds?.length,
+                  singular: 'identifier',
+                  target: 'sap_sids',
+                  onClick: () => {
+                    handleClick(
+                      'SAP IDs (SID)',
+                      generalMapper(properties.sapIds, 'SID')
+                    );
+                  },
+                },
+              ]
+            : []),
+          ...(hasSystemPurpose
+            ? [{ title: 'System purpose', value: properties.systemPurpose }]
+            : []),
+          ...(hasCPUs
+            ? [{ title: 'Number of CPUs', value: properties.cpuNumber }]
+            : []),
+          ...(hasSockets
+            ? [{ title: 'Sockets', value: properties.sockets }]
+            : []),
+          ...(hasCores
+            ? [
+                {
+                  title: 'Cores per socket',
+                  value: properties.coresPerSocket,
+                },
+              ]
+            : []),
+          ...(hasCPUFlags
+            ? [
+                {
+                  title: 'CPU flags',
+                  value: properties?.cpuFlags?.length,
+                  singular: 'flag',
+                  target: 'flag',
+                  onClick: () =>
+                    handleClick(
+                      'CPU flags',
+                      generalMapper(properties.cpuFlags, 'flag name')
+                    ),
+                },
+              ]
+            : []),
+          ...(hasRAM ? [{ title: 'RAM', value: properties.ramSize }] : []),
+          ...extra.map(({ onClick, ...item }) => ({
+            ...item,
+            ...(onClick && { onClick: (e) => onClick(e, handleClick) }),
+          })),
+        ]}
+      />
+      <TextInputModal
+        isOpen={isDisplayNameModalOpen}
+        title="Edit display name"
+        value={entity && entity.display_name}
+        ariaLabel="Host inventory display name"
+        modalOuiaId="edit-display-name-modal"
+        cancelOuiaId="cancel-edit-display-name"
+        confirmOuiaId="confirm-edit-display-name"
+        inputOuiaId="input-edit-display-name"
+        onCancel={onCancel}
+        onSubmit={onSubmit(setDisplayName, entity && entity.display_name)}
+        className="sentry-mask data-hj-suppress"
+      />
+      <TextInputModal
+        isOpen={isAnsibleHostModalOpen}
+        title="Edit Ansible host"
+        value={entity && getAnsibleHost()}
+        ariaLabel="Ansible host"
+        modalOuiaId="edit-ansible-name-modal"
+        cancelOuiaId="cancel-edit-ansible-name"
+        confirmOuiaId="confirm-edit-ansible-name"
+        inputOuiaId="input-edit-ansible-name"
+        onCancel={onCancel}
+        onSubmit={onSubmit(setAnsibleHost, entity && getAnsibleHost())}
+        className="sentry-mask data-hj-suppress"
+      />
+    </Fragment>
+  );
+};
 
-    return (
-      <Fragment>
-        <LoadingCard
-          title="System properties"
-          isLoading={!detailLoaded}
-          cardId="system-card"
-          items={[
-            ...(hasHostName
-              ? [
-                  {
-                    title: (
-                      <TitleWithPopover
-                        title="Host name"
-                        content="Name imported from the system."
-                      />
-                    ),
-                    value: entity.fqdn,
-                    size: 'md',
-                    customClass: 'sentry-mask data-hj-suppress',
-                  },
-                ]
-              : []),
-            ...(hasDisplayName
-              ? [
-                  {
-                    title: (
-                      <TitleWithPopover
-                        title="Display name"
-                        content="System name displayed in an inventory list."
-                      />
-                    ),
-                    value: (
-                      <Fragment>
-                        {entity.display_name}
-                        <EditButton
-                          writePermissions={writePermissions}
-                          link="display_name"
-                          onClick={this.onShowDisplayModal}
-                        />
-                      </Fragment>
-                    ),
-                    size: 'md',
-                    customClass: 'sentry-mask data-hj-suppress',
-                  },
-                ]
-              : []),
-            ...(hasAnsibleHostname
-              ? [
-                  {
-                    title: (
-                      <TitleWithPopover
-                        title="Ansible hostname"
-                        content="Hostname that is used in playbooks by Remediations."
-                      />
-                    ),
-                    value: (
-                      <Fragment>
-                        {this.getAnsibleHost()}
-                        <EditButton
-                          writePermissions={writePermissions}
-                          link="ansible_name"
-                          onClick={this.onShowAnsibleModal}
-                        />
-                      </Fragment>
-                    ),
-                    size: 'md',
-                    customClass: 'sentry-mask data-hj-suppress',
-                  },
-                ]
-              : []),
-            ...(hasWorkspace
-              ? [
-                  {
-                    title: 'Workspace',
-                    value:
-                      entity.groups?.length > 0 && entity.groups?.[0]?.name,
-                    size: 'md',
-                    customClass: 'sentry-mask data-hj-suppress',
-                    target: `/workspaces/${entity.groups?.[0]?.id}`,
-                  },
-                ]
-              : []),
-            ...(hasSAP
-              ? [
-                  {
-                    title: 'SAP',
-                    value: properties.sapIds?.length,
-                    singular: 'identifier',
-                    target: 'sap_sids',
-                    onClick: () => {
-                      handleClick(
-                        'SAP IDs (SID)',
-                        generalMapper(properties.sapIds, 'SID')
-                      );
-                    },
-                  },
-                ]
-              : []),
-            ...(hasSystemPurpose
-              ? [{ title: 'System purpose', value: properties.systemPurpose }]
-              : []),
-            ...(hasCPUs
-              ? [{ title: 'Number of CPUs', value: properties.cpuNumber }]
-              : []),
-            ...(hasSockets
-              ? [{ title: 'Sockets', value: properties.sockets }]
-              : []),
-            ...(hasCores
-              ? [
-                  {
-                    title: 'Cores per socket',
-                    value: properties.coresPerSocket,
-                  },
-                ]
-              : []),
-            ...(hasCPUFlags
-              ? [
-                  {
-                    title: 'CPU flags',
-                    value: properties?.cpuFlags?.length,
-                    singular: 'flag',
-                    target: 'flag',
-                    onClick: () =>
-                      handleClick(
-                        'CPU flags',
-                        generalMapper(properties.cpuFlags, 'flag name')
-                      ),
-                  },
-                ]
-              : []),
-            ...(hasRAM ? [{ title: 'RAM', value: properties.ramSize }] : []),
-            ...extra.map(({ onClick, ...item }) => ({
-              ...item,
-              ...(onClick && { onClick: (e) => onClick(e, handleClick) }),
-            })),
-          ]}
-        />
-        <TextInputModal
-          isOpen={isDisplayNameModalOpen}
-          title="Edit display name"
-          value={entity && entity.display_name}
-          ariaLabel="Host inventory display name"
-          modalOuiaId="edit-display-name-modal"
-          cancelOuiaId="cancel-edit-display-name"
-          confirmOuiaId="confirm-edit-display-name"
-          inputOuiaId="input-edit-display-name"
-          onCancel={this.onCancel}
-          onSubmit={this.onSubmit(
-            setDisplayName,
-            entity && entity.display_name
-          )}
-          className="sentry-mask data-hj-suppress"
-        />
-        <TextInputModal
-          isOpen={isAnsibleHostModalOpen}
-          title="Edit Ansible host"
-          value={entity && this.getAnsibleHost()}
-          ariaLabel="Ansible host"
-          modalOuiaId="edit-ansible-name-modal"
-          cancelOuiaId="cancel-edit-ansible-name"
-          confirmOuiaId="confirm-edit-ansible-name"
-          inputOuiaId="input-edit-ansible-name"
-          onCancel={this.onCancel}
-          onSubmit={this.onSubmit(
-            setAnsibleHost,
-            entity && this.getAnsibleHost()
-          )}
-          className="sentry-mask data-hj-suppress"
-        />
-      </Fragment>
-    );
-  }
-}
-
-SystemCardCore.propTypes = {
-  detailLoaded: PropTypes.bool,
-  entity: PropTypes.shape({
-    // eslint-disable-next-line camelcase
-    display_name: PropTypes.string,
-    // eslint-disable-next-line camelcase
-    ansible_host: PropTypes.string,
-    fqdn: PropTypes.string,
-    id: PropTypes.string,
-  }),
-  properties: PropTypes.shape({
-    cpuNumber: PropTypes.number,
-    sockets: PropTypes.number,
-    coresPerSocket: PropTypes.number,
-    ramSize: PropTypes.string,
-    storage: PropTypes.arrayOf(
-      PropTypes.shape({
-        device: PropTypes.string,
-        // eslint-disable-next-line camelcase
-        mount_point: PropTypes.string,
-        options: PropTypes.shape({}),
-        type: PropTypes.string,
-      })
-    ),
-    sapIds: PropTypes.arrayOf(PropTypes.string),
-    systemPurpose: PropTypes.string,
-    cpuFlags: PropTypes.array,
-  }),
+SystemCard.propTypes = {
   setDisplayName: PropTypes.func,
   setAnsibleHost: PropTypes.func,
   writePermissions: PropTypes.bool,
@@ -281,6 +237,7 @@ SystemCardCore.propTypes = {
   hasHostName: PropTypes.bool,
   hasDisplayName: PropTypes.bool,
   hasAnsibleHostname: PropTypes.bool,
+  hasWorkspace: PropTypes.bool,
   hasSAP: PropTypes.bool,
   hasSystemPurpose: PropTypes.bool,
   hasCPUs: PropTypes.bool,
@@ -290,7 +247,7 @@ SystemCardCore.propTypes = {
   hasRAM: PropTypes.bool,
   extra: PropTypes.arrayOf(extraShape),
 };
-SystemCardCore.defaultProps = {
+SystemCard.defaultProps = {
   detailLoaded: false,
   entity: {},
   properties: {},
@@ -307,29 +264,5 @@ SystemCardCore.defaultProps = {
   hasRAM: true,
   extra: [],
 };
-
-function mapDispatchToProps(dispatch) {
-  return {
-    setDisplayName: (id, value, origValue) => {
-      dispatch(editDisplayName(id, value, origValue));
-    },
-
-    setAnsibleHost: (id, value, origValue) => {
-      dispatch(editAnsibleHost(id, value, origValue));
-    },
-  };
-}
-
-export const SystemCard = connect(
-  ({ entityDetails: { entity }, systemProfileStore: { systemProfile } }) => ({
-    entity,
-    detailLoaded: systemProfile && systemProfile.loaded,
-    properties: propertiesSelector(systemProfile, entity),
-  }),
-  mapDispatchToProps
-)(SystemCardCore);
-
-SystemCard.propTypes = SystemCardCore.propTypes;
-SystemCard.defaultProps = SystemCardCore.defaultProps;
 
 export default SystemCard;
