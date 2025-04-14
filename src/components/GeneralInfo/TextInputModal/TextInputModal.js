0ccb9c642338a5382f4e137e9579186cb22bab3b
Hugo Hezel <hhezel@redhat.com>
2025-04-14 15:55:42 +0200

    feat(RHINENG-17384): Convert class component TextInputModal into functional one

--- src/components/GeneralInfo/TextInputModal/TextInputModal.js
+++ src/components/GeneralInfo/TextInputModal/TextInputModal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -11,95 +11,79 @@ import {
   ValidatedOptions,
 } from '@patternfly/react-core';
 
-export default class TextInputModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const TextInputModal = ({
+  title = '',
+  isOpen = false,
+  onCancel = () => undefined,
+  onSubmit = () => undefined,
+  ariaLabel = 'input text',
+  modalOuiaId,
+  cancelOuiaId,
+  confirmOuiaId,
+  inputOuiaId,
+  value: initialValue,
+}) => {
+  const [value, setValue] = useState(initialValue || '');
 
-  static getDerivedStateFromProps(props, state) {
-    if (!props.isOpen) {
-      return { value: undefined };
+  useEffect(() => {
+    if (!isOpen) {
+      setValue(undefined);
+    } else if (value === undefined) {
+      setValue(initialValue || '');
     }
+  }, [isOpen, initialValue, value]);
 
-    if (state.value !== undefined) {
-      return null;
-    }
-
-    return {
-      value: props.value || '',
-    };
-  }
-
-  render() {
-    const {
-      title,
-      isOpen,
-      onCancel,
-      onSubmit,
-      ariaLabel,
-      modalOuiaId,
-      cancelOuiaId,
-      confirmOuiaId,
-      inputOuiaId,
-    } = this.props;
-    const { value } = this.state;
-
-    return (
-      <Modal
-        variant="small"
-        title={title}
-        className="ins-c-inventory__detail--edit"
-        aria-label={ariaLabel ? `${ariaLabel} - modal` : 'input modal'}
-        ouiaId={modalOuiaId}
-        isOpen={isOpen}
-        onClose={(event) => onCancel(event)}
-        actions={[
-          <Button
-            key="confirm"
-            data-action="confirm"
-            variant="primary"
-            onClick={() => onSubmit(this.state.value?.trim())}
-            ouiaId={confirmOuiaId}
-            isDisabled={
-              this.props.value === this.state.value ||
-              this.state.value?.trim().length === 0
-            }
-          >
-            Save
-          </Button>,
-          <Button
-            key="cancel"
-            data-action="cancel"
-            variant="link"
-            onClick={onCancel}
-            ouiaId={cancelOuiaId}
-          >
-            Cancel
-          </Button>,
-        ]}
-      >
-        <TextInput
-          value={value}
-          type="text"
-          ouiaId={inputOuiaId}
-          onChange={(_event, value) => this.setState({ value })}
-          aria-label={ariaLabel}
-          validated={value?.trim().length === 0 && ValidatedOptions.error}
-        />
-        <FormHelperText>
-          <HelperText id="helper-text2" aria-live="polite">
-            {value?.trim().length === 0 && (
-              <HelperTextItem variant="error">
-                Name cannot be blank
-              </HelperTextItem>
-            )}
-          </HelperText>
-        </FormHelperText>
-      </Modal>
-    );
-  }
-}
+  return (
+    <Modal
+      variant="small"
+      title={title}
+      className="ins-c-inventory__detail--edit"
+      aria-label={ariaLabel ? `${ariaLabel} - modal` : 'input modal'}
+      ouiaId={modalOuiaId}
+      isOpen={isOpen}
+      onClose={(event) => onCancel(event)}
+      actions={[
+        <Button
+          key="confirm"
+          data-action="confirm"
+          variant="primary"
+          onClick={() => onSubmit(value?.trim())}
+          ouiaId={confirmOuiaId}
+          isDisabled={value === initialValue || value?.trim().length === 0}
+        >
+          Save
+        </Button>,
+        <Button
+          key="cancel"
+          data-action="cancel"
+          variant="link"
+          onClick={onCancel}
+          ouiaId={cancelOuiaId}
+        >
+          Cancel
+        </Button>,
+      ]}
+    >
+      <TextInput
+        value={value}
+        type="text"
+        ouiaId={inputOuiaId}
+        onChange={(_event, value) => setValue(value)}
+        aria-label={ariaLabel}
+        validated={value?.trim().length === 0 && ValidatedOptions.error}
+      />
+      <FormHelperText>
+        <HelperText id="helper-text2" aria-live="polite">
+          {value?.trim().length === 0 && (
+            <HelperTextItem variant="error">
+              Name cannot be blank
+            </HelperTextItem>
+          )}
+        </HelperText>
+      </FormHelperText>
+    </Modal>
+  );
+};
 
 TextInputModal.propTypes = {
   title: PropTypes.string,
@@ -114,10 +98,4 @@ TextInputModal.propTypes = {
   value: PropTypes.string,
 };
 
-TextInputModal.defaultProps = {
-  onCancel: () => undefined,
-  onSubmit: () => undefined,
-  isOpen: false,
-  title: '',
-  ariaLabel: 'input text',
-};
+export default TextInputModal;
