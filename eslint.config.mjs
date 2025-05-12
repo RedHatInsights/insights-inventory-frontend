fd6f8e8570826cfdbc5ac5514ed666e61d1008d5
Georgii Karataev <georgkarat@gmail.com>
2025-05-12 12:22:26 +0200

    chore(deps): Upgrade ESLint to version 9, change configs

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import fecPlugin from '@redhat-cloud-services/eslint-config-redhat-cloud-services';
+import tsParser from '@typescript-eslint/parser';
+import pluginCypress from 'eslint-plugin-cypress/flat';
+import jestDom from 'eslint-plugin-jest-dom';
+import jsdoc from 'eslint-plugin-jsdoc';
+import reactHooks from 'eslint-plugin-react-hooks';
+import testingLibrary from 'eslint-plugin-testing-library';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['node_modules/*', 'static/*', 'dist/*', 'docs/*']),
+  fecPlugin,
+  pluginCypress.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  jsdoc.configs['flat/recommended'],
+  tseslint.configs.recommended,
+  testingLibrary.configs['flat/react'],
+  jestDom.configs['flat/recommended'],
+  {
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      'rulesdir/disallow-fec-relative-imports': 'off',
+      '@typescript-eslint/no-unused-vars': 1,
+      'rulesdir/forbid-pf-relative-imports': 'off',
+      'jsdoc/tag-lines': 0,
+      'jsdoc/require-jsdoc': 0,
+
+      'jsdoc/check-line-alignment': [
+        'error',
+        'always',
+        {
+          customSpacings: {
+            postDelimiter: 2,
+          },
+        },
+      ],
+
+      'jsdoc/check-tag-names': [
+        'warn',
+        {
+          definedTags: ['category', 'subcategory'],
+        },
+      ],
+    },
+  },
+]);
