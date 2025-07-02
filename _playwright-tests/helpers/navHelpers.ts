6cab706331019ce37d661def894268f28d078f16
Adarsh Dubey <addubey@redhat.com>
2025-07-02 21:26:17 +0530

    feat(RHINENG-19078): Playwright onboarding

---
+++ _playwright-tests/helpers/navHelpers.ts
@@ -0,0 +1,7 @@
+import { type Page } from '@playwright/test';
+
+export const navigateToInventoryFunc = async (page: Page) => {
+  await page.goto('/insights/inventory/', { timeout: 20000 });
+};
+
+
