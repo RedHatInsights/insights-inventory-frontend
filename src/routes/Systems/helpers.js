44e497d06aac0a1a7fcd34889a73ce3604f30b60
Rastislav Hepner <rhepner@redhat.com>
2025-08-11 17:32:48 +0200

    WIP: add filter definitions

--- src/routes/Systems/helpers.js
+++ src/routes/Systems/helpers.js
@@ -28,14 +28,22 @@ export const fetchSystems = async (serialisedTableState) => {
       'bootc_status',
     ],
   };
+  const { filter, ...filterParams } = serialisedTableState?.filters || {};
 
   const params = {
     // TODO Add global filter
     ...(serialisedTableState?.pagination || {}),
-    ...(serialisedTableState?.filters || {}),
     ...(serialisedTableState?.sort || {}),
+    // These are "filters" that are set via a specific URL parameter, like hostname, systemType, staleness, etc.
+    ...filterParams,
     options: {
-      params: generateFilter(fields, 'fields'),
+      params: {
+        // the "generateFilter" function is used here, because coincidentally the "fields URL parameter(s)" behave similar to the filter
+        ...generateFilter(fields, 'fields'),
+        // The rest are those that need to be a filter query parameter, like OS version or name and other "field filters"
+        // "filter", is the object of the merged "filter" prop of the individual object returned from each filter(serialiser)
+        ...generateFilter(filter),
+      },
     },
   };
 
@@ -67,4 +75,27 @@ export const resolveFilters = (filters) => {
   } else {
     return filters;
   }
+};
+
+const osVersionSorter = (a, b) =>
+  b.major === a.major ? b.minor - a.minor : b.major - a.major;
+
+export const getOsSelectOptions = (osName, osData) => {
+  if (!osData) return [];
+
+  const osItems = osData
+    .filter((item) => item.name === osName)
+    .toSorted(osVersionSorter);
+  const majors = [...new Set(osItems.map((item) => item.major))];
+
+  return majors.map((major) => ({
+    label: `${osName} ${major}`,
+    value: `${osName}`,
+    items: osItems
+      .filter((item) => item.major === major)
+      .map((item) => ({
+        label: `${item.name} ${item.major}.${item.minor}`,
+        value: `${item.major}.${item.minor}`,
+      })),
+  }));
 };
