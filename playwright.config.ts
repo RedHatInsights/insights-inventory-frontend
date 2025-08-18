059234b393bf3c3562ae8221b3ba331703a88c4f
Adarsh Dubey <addubey@redhat.com>
2025-08-18 20:26:14 +0530

    ci(playwright): add PR checks pipeline for test automationn

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,21 +2,19 @@ import { defineConfig, devices } from '@playwright/test';
 import 'dotenv/config';
 
 export default defineConfig({
-  testDir: './_playwright-tests',
-  /* Run tests in files in parallel */
+  testDir: './_playwright-tests/',
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [
+    ['list'],
+    ['json', { outputFile: 'playwright-ctrf/playwright-ctrf.json' }],
+    ['html', { outputFolder: 'playwright-report' }],
+  ],
   timeout: 90_000,
   use: {
-    actionTimeout: 90_000, // 90s
+    actionTimeout: 90_000,
     navigationTimeout: 90_000,
     headless: true,
     baseURL: process.env.BASE_URL,
@@ -24,7 +22,6 @@ export default defineConfig({
     trace: 'on',
     ignoreHTTPSErrors: true,
   },
-
   projects: [
     { name: 'setup', testMatch: /.*\.setup\.ts/ },
     {
