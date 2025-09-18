288f3cbdf3b5a731ebcb90fd1cd168ea0540741f
Adarsh Dubey <addubey@redhat.com>
2025-09-18 17:19:11 +0530

    feat(RHINENG-20554): Add workspace test case for create,rename,delete

--- _playwright-tests/helpers/navHelpers.ts
+++ _playwright-tests/helpers/navHelpers.ts
@@ -3,5 +3,10 @@ import { test, expect } from '@playwright/test';
 
 export const navigateToInventoryFunc = async (page: Page) => {
   await page.goto('/insights/inventory/', { timeout: 100000 });
-  await expect(page.getByRole('heading', { name: 'Systems' })).toBeVisible({ timeout: 50000 });
+  await expect(page.getByRole('heading', { name: 'Systems' })).toBeVisible({ timeout: 100000 });
+};
+
+export const navigateToWorkspacesFunc = async (page: Page) => {
+  await page.goto('/insights/inventory/workspaces', { timeout: 100000 });
+  await expect(page.getByRole('heading', { name: 'Workspaces' })).toBeVisible({ timeout: 100000 });
 };
