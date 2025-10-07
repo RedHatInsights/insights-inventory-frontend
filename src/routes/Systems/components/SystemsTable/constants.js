6a65255bc574eca3453be7f75dbd72c69462fe2b
Sebastian Gräßl <bastilian@users.noreply.github.com>
2025-10-07 13:19:05 +0200

    refactor(SystemsTable): Improve integrate-ability and customising (#2670)

--- src/routes/Systems/components/SystemsTable/constants.js
+++ src/routes/Systems/components/SystemsTable/constants.js
@@ -1,6 +1,17 @@
-export const DEFAULT_OPTIONS = {
-  perPage: 50,
-  onSelect: () => true,
-};
+// TODO Move serialisers.js within SystemsTable directory
+import {
+  filtersSerialiser,
+  sortSerialiser,
+  paginationSerialiser,
+} from '../../serialisers';
 
-export const MAX_PER_PAGE = 100;
+export const DEFAULT_OPTIONS = {
+  // FIXME: remove debug
+  debug: true,
+  perPage: 50,
+  serialisers: {
+    pagination: paginationSerialiser,
+    sort: sortSerialiser,
+    filters: filtersSerialiser,
+  },
+};
