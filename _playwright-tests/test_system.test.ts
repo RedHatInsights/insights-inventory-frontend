8c0023bf921c7c91cf08976af35cf7b65b46a6d8
zhamilya <@computercamplove>
2025-10-09 16:17:59 +0200

    test: add E2E test for edit system and delete from Details page

--- _playwright-tests/test_system.test.ts
+++ _playwright-tests/test_system.test.ts
@@ -18,6 +18,7 @@ test('User should be able to edit and delete a system from Systems page', async 
     const rhel94VM = 'iqe-vm-cli-80196d0a-2704-4b38-bad0-8eacc6feda7d';
     const rhel94VMRenamed = `${rhel94VM}_Renamed`;
     const dialog = page.locator('[role="dialog"]');
+    
 
     await test.step('Upload the archive required for the test', async () => {
         uploadArchive('rhel94_core_collect.tar.gz');
@@ -61,60 +62,61 @@ test('User should be able to edit and delete a system from Systems page', async 
 });
 
 
-test('User should be able to edit and delete a system from Systems page', async ({ page }) => {
-    /** 
-     * Jira References:
-        - https://issues.redhat.com/browse/RHINENG-21146 – Create a system
-        - https://issues.redhat.com/browse/RHINENG-21147 – Edit a system
-        - https://issues.redhat.com/browse/RHINENG-21149 - Delete a system
-    * Metadata:
-        - requirements:
-            - inv-hosts-patch
-            - inv-hosts-delete-by-id
-        - importance: critical
-        - assignee: zabikeno
-    */
-    const rhel94VM = 'iqe-vm-cli-80196d0a-2704-4b38-bad0-8eacc6feda7d';
-    const rhel94VMRenamed = `${rhel94VM}_Renamed`;
-    const dialog = page.locator('[role="dialog"]');
+test('User should be able to edit and delete a system from System Details page', async ({ page }) => {
+/** 
+ * Jira References:
+    - https://issues.redhat.com/browse/RHINENG-21147 – Edit a system
+    - https://issues.redhat.com/browse/RHINENG-21149 - Delete a system
+* Metadata:
+    - requirements:
+        - inv-hosts-patch
+        - inv-hosts-delete-by-id
+    - importance: critical
+    - assignee: zabikeno
+*/
+    const hostName = 'iqe-vm-cli-80196d0a-2704-4b38-bad0-8eacc6feda7d';
+    const editButtons = page.getByRole('button', { name: 'Edit' });
+    const newDisplayName = `host_${Date.now()}_${Math.floor(Math.random() * 1000)}`;
+    const newAnsibleName = `host_${Date.now()}_${Math.floor(Math.random() * 1000)}`;
+    const dialogModal = page.locator('[role="dialog"]');
 
-    await test.step('Upload the archive required for the test', async () => {
+    await test.step('Setup: upload the archive required for the test and navigate to system', async () => {
         uploadArchive('rhel94_core_collect.tar.gz');
-    });
-
-    await test.step('Navigate to Inventory → Systems', async () => {
         await navigateToInventorySystemsFunc(page);
+        await searchByName(page, hostName);
+
+        const systemLink = page.getByRole('link', { name: hostName });
+        await expect(systemLink).toBeVisible({ timeout: 100000 });
+        await systemLink.click();
+        await expect(page.getByRole('heading', { name: hostName })).toBeVisible({ timeout: 100000 });
     });
 
-    await test.step(`Search for the system "${rhel94VM}"`, async () => {
-        await searchByName(page, rhel94VM);
+    await test.step('Edit the system display name and verify', async () => {
+        await editButtons.nth(0).click();
+        await page.locator('[aria-label="name"]').first().fill(newDisplayName);
+        await page.getByRole('button', { name: 'submit' }).click();
+
+        await expect(page.getByRole('heading', { name: newDisplayName })).toBeVisible();
+        const displayNameValueLocator = page.getByLabel('Display name value');
+        await expect(displayNameValueLocator).toHaveText(newDisplayName);
     });
 
-    await test.step('Edit the system display name and save', async () => {
-        await page
-        .getByRole('row', { name: new RegExp(rhel94VM, 'i') })
-        .getByLabel('Kebab toggle')
-        .click();
-        await page.getByRole('menuitem', { name: 'Edit display name' }).first().click();
+    await test.step('Edit the system Ansible name and verify', async () => {
+        await editButtons.nth(1).click();
+        await page.locator('[aria-label="name"]').first().fill(newAnsibleName);
+        await page.getByRole('button', { name: 'submit' }).click();
 
-        await expect(dialog).toBeVisible();
-        await dialog.locator('input').first().fill(rhel94VMRenamed);
-        await dialog.getByRole('button', { name: 'Save' }).click();
+        const ansibleNameValueLocator = page.getByLabel('Ansible hostname value');
+        await expect(ansibleNameValueLocator).toHaveText(newAnsibleName);
     });
 
     await test.step(`Delete the renamed system and verify it is removed`, async () => {
-        await searchByName(page, rhel94VMRenamed);
+        await page.getByRole('button', { name: 'Delete' }).click();
+        await expect(dialogModal).toBeVisible();
+        await dialogModal.getByRole('button', { name: 'Delete' }).click();
 
-        await page
-        .getByRole('row', { name: new RegExp(rhel94VMRenamed, 'i') })
-        .getByLabel('Kebab toggle')
-        .click();
-
-        await page.getByRole('menuitem', { name: 'Delete from inventory' }).first().click();
-        await expect(dialog).toBeVisible();
-        await dialog.getByRole('button', { name: 'Delete' }).click();
-
-        await searchByName(page, rhel94VMRenamed);
+        await navigateToInventorySystemsFunc(page);
+        await searchByName(page, newDisplayName);
         await expect(page.getByText('No matching systems found')).toBeVisible();
   });
 });
