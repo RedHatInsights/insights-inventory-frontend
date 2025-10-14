1e7835de73d4dd3dd8242b4fcfbc88eaf011cba5
Adarsh Dubey <addubey@redhat.com>
2025-10-14 15:31:23 +0530

    fix: Improving the root locators for navigation (#2692)
    Co-authored-by: Zhamilya Abikenova <zabikeno@redhat.com>

--- _playwright-tests/helpers/navHelpers.ts
+++ _playwright-tests/helpers/navHelpers.ts
@@ -9,6 +9,7 @@ import { closePopupsIfExist } from './loginHelpers';
 export const navigateToInventorySystemsFunc = async (page: Page) => {
   await page.goto('/insights/inventory/', { timeout: 100000 });
   await expect(page.getByRole('heading', { name: 'Systems' })).toBeVisible({ timeout: 100000 });
+  await page.waitForSelector('#options-menu-bottom-toggle', { state: 'visible' });
 };
 
 /**
@@ -18,6 +19,8 @@ export const navigateToInventorySystemsFunc = async (page: Page) => {
 export const navigateToWorkspacesFunc = async (page: Page) => {
   await page.goto('/insights/inventory/workspaces', { timeout: 100000 });
   await expect(page.getByRole('heading', { name: 'Workspaces' })).toBeVisible({ timeout: 100000 });
+  await page.waitForSelector('#pagination-options-menu-bottom-bottom-toggle', { state: 'visible' });
+
 };
 
 export const test = base.extend({
