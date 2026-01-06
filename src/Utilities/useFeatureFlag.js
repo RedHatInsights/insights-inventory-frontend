aef92520e796ca7f1c0a6f60f9a4d3d506abf11b
zhamilya <@computercamplove>
2026-01-06 16:29:08 +0100

    feat(RHINENG-22893): Add Outage Alert component

--- src/Utilities/useFeatureFlag.js
+++ src/Utilities/useFeatureFlag.js
@@ -1,7 +1,33 @@
-import { useFlag, useFlagsStatus } from '@unleash/proxy-client-react';
+import {
+  useFlag,
+  useFlagsStatus,
+  useVariant,
+} from '@unleash/proxy-client-react';
 
 export default (flag) => {
   const { flagsReady } = useFlagsStatus();
   const isFlagEnabled = useFlag(flag);
   return flagsReady ? isFlagEnabled : undefined;
+};
+
+export const useFeatureVariant = (flag) => {
+  const { flagsReady } = useFlagsStatus();
+  const variant = useVariant(flag);
+
+  const isEnabled = flagsReady ? variant.enabled : false;
+
+  let payload = undefined;
+  if (flagsReady && variant.payload) {
+    try {
+      payload = JSON.parse(variant.payload.value);
+    } catch {
+      payload = undefined;
+    }
+  }
+
+  return {
+    isEnabled,
+    body: payload?.body,
+    title: payload?.title,
+  };
 };
