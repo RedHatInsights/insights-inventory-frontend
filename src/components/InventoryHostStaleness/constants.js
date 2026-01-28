be1b888070be6638a2a3ce4e29a84cd9b5f24f00
Jerome Marc <jerome.marc@gmail.com>
2026-01-28 10:28:34 +0100

    Clarify organization level settings in popover (RHINENG-22644)
    Updated text in the popover to clarify organization level settings and system staleness.
    
    RHINENG-22644

--- src/components/InventoryHostStaleness/constants.js
+++ src/components/InventoryHostStaleness/constants.js
@@ -165,21 +165,20 @@ export const InventoryHostStalenessPopover = () => {
     <Popover
       aria-label="Organization level popover"
       headerContent={
-        <Title headingLevel="h4">Organization level setting</Title>
+        <Title headingLevel="h4">About system staleness and deletion</Title>
       }
       hasAutoWidth
       position="top"
       bodyContent={
         <Flex direction={{ default: 'column' }}>
           <p className="pf-v6-u-font-size-sm">
-            Configure the number of days it will take for your systems to be
-            marked as stale, stale warning, and be deleted.
+            Configure how long systems can go without checking in before they are flagged or removed from your inventory. These settings apply to all systems in your organization.
           </p>
           <Flex
             direction={{ default: 'column' }}
             spaceItems={{ default: 'spaceItemsNone' }}
           >
-            <span className="pf-v6-u-font-size-sm">Default settings are:</span>
+            <span className="pf-v6-u-font-size-sm">Default settings:</span>
             <span className="pf-v6-u-font-size-sm">
               <p>
                 - Systems are marked as stale after 1 day since last check-in.
@@ -193,6 +192,18 @@ export const InventoryHostStalenessPopover = () => {
               - Systems are deleted after 30 days since last check-in.
             </span>
           </Flex>
+          <span>
+            <a
+              href={
+                'https://docs.redhat.com/en/documentation/red_hat_lightspeed/1-latest/html-single/viewing_and_managing_system_inventory/index#systems-lifecycle_user-access'
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Managing system staleness and deletion
+              <ExternalLinkAltIcon aria-hidden="true" focusable="false" className="pf-v6-u-ml-xs" />
+            </a>
+          </span>
         </Flex>
       }
     >
