5b72a3c0857885052eea9088d00d86d1009cd136
Jerome Marc <jerome.marc@gmail.com>
2026-01-28 11:08:06 +0100

    fix: Refactor Popover content with React fragment
    Updated the Popover component with React fragment <>...</>

--- src/components/InventoryGroups/SmallComponents/Popover.js
+++ src/components/InventoryGroups/SmallComponents/Popover.js
@@ -14,28 +14,30 @@ const InventoryGroupsPopover = () => (
     headerContent={<Title headingLevel="h4">About workspaces</Title>}
     position="right"
     bodyContent={
-      <Content>
-        <Content component={ContentVariants.p}>
-          Workspaces allow you to select specific systems and group them
-          together to better organize your inventory.
+      <>
+        <Content>
+          <Content component={ContentVariants.p}>
+            Workspaces allow you to select specific systems and group them
+            together to better organize your inventory.
+          </Content>
+          <Content component={ContentVariants.p}>
+            You can also manage user access to specific workspaces to enhance
+            security within your organization.
+          </Content>
         </Content>
-        <Content component={ContentVariants.p}>
-          You can also manage user access to specific workspaces to enhance
-          security within your organization.
-        </Content>
-      </Content>
-      <span>
-        <a
-          href={
-            'https://docs.redhat.com/en/documentation/red_hat_lightspeed/1-latest/html/viewing_and_managing_system_inventory/deploying-insights-with-rhca_user-access'
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn more about managing workspaces and user access
-          <ExternalLinkAltIcon aria-hidden="true" focusable="false" className="pf-v6-u-ml-xs" />
-        </a>
-      </span>
+        <span>
+          <a
+            href={
+              'https://docs.redhat.com/en/documentation/red_hat_lightspeed/1-latest/html/viewing_and_managing_system_inventory/deploying-insights-with-rhca_user-access'
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn more about managing workspaces and user access
+            <ExternalLinkAltIcon aria-hidden="true" focusable="false" className="pf-v6-u-ml-xs" />
+          </a>
+        </span>
+      </>
     }
   >
     <Button
