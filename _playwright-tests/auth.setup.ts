31f78ed11369478d2bccaec9c2e8ec4a42d54c95
Rastislav Hepner <rhepner@redhat.com>
2026-01-30 18:43:50 +0100

    test: auto-close TrustArc cookie consent banner

--- _playwright-tests/auth.setup.ts
+++ _playwright-tests/auth.setup.ts
@@ -5,6 +5,7 @@ import {
   storeStorageStateAndToken,
   throwIfMissingEnvVariables,
   closePopupsIfExist,
+  closeCookieBanner,
 } from './helpers/loginHelpers';
 
 setup.describe('Setup', async () => {
@@ -19,6 +20,7 @@ setup.describe('Setup', async () => {
 
     await closePopupsIfExist(page);
     await logInWithUser1(page);
+    await closeCookieBanner(page);
     await storeStorageStateAndToken(page);
 
     await ensureNotInPreview(page);
