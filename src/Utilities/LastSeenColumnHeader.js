24ceb3e7d68edc9486b71805accc68ba93faf09a
zhamilya <@computercamplove>
2026-03-18 13:45:49 +0100

    fix(SystemsView): Fix Last seen column name and display toolpit

--- src/Utilities/LastSeenColumnHeader.js
+++ src/Utilities/LastSeenColumnHeader.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Flex, FlexItem, Icon, Tooltip } from '@patternfly/react-core';
+import { Icon, Tooltip } from '@patternfly/react-core';
 import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
 
 export const LastSeenColumnHeader = () => {
   return (
-    <Flex display={{ default: 'inlineFlex' }}>
-      <FlexItem spacer={{ default: 'spacerXs' }}>Last seen</FlexItem>
-      <FlexItem>
-        <Tooltip
-          content={`Last seen represents the most recent time a system
+    <span>
+      Last seen
+      <Tooltip
+        content={`Last seen represents the most recent time a system
           checked in and uploaded sufficient data for Red Hat Lightspeed analysis.
           The timestamps may vary between applications as they rely on
           different data collectors.`}
-        >
-          <Icon status="custom">
-            <OutlinedQuestionCircleIcon color="var(--pf-t--global--icon--color--subtle)" />
-          </Icon>
-        </Tooltip>
-      </FlexItem>
-    </Flex>
+      >
+        <Icon status="custom" style={{ marginLeft: '3px' }}>
+          <OutlinedQuestionCircleIcon color="var(--pf-t--global--icon--color--subtle)" />
+        </Icon>
+      </Tooltip>
+    </span>
   );
 };
