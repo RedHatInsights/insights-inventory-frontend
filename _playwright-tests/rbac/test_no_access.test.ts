f6495b60934fb405934ce07a21839e92b5b59538
zhamilya <@computercamplove>
2026-04-13 14:58:16 +0200

    test: add tests

--- _playwright-tests/rbac/test_no_access.test.ts
+++ _playwright-tests/rbac/test_no_access.test.ts
@@ -8,19 +8,25 @@ const NO_ACCESS_STALENESS = 'Access permissions needed';
 
 test.describe('No access: @rbac', () => {
   test('Systems page', async ({ page }) => {
-    await page.goto('/insights/inventory/', { timeout: 10000 });
+    await page.goto('/insights/inventory/', { timeout: 100000 });
 
     const emptyState = page.locator(
       '[data-ouia-component-id="UnauthorizedAccess"]',
     );
-    await expect(emptyState).toBeVisible({ timeout: 10000 });
+    await expect(emptyState).toBeVisible({ timeout: 100000 });
 
     const heading = page.getByRole('heading', { level: 5 }).first();
     await expect(heading).toContainText(NO_ACCESS_INVENTORY);
   });
 
   test('Workspaces page', async ({ page }) => {
-    await page.goto('/insights/inventory/workspaces', { timeout: 10000 });
+    await page.goto('/insights/inventory/workspaces', { timeout: 100000 });
+
+    const createButton = page.locator(
+      '[data-ouia-component-id="CreateGroupButton"]',
+    );
+    await expect(createButton).toBeVisible({ timeout: 100000 });
+
     const heading = page.getByRole('heading', { level: 4 }).first();
     await expect(heading).toContainText('No workspaces');
     // TODO: uncomment when RBAC v2 check is resolved
@@ -35,8 +41,11 @@ test.describe('No access: @rbac', () => {
 
   test('Staleness and Deletion page', async ({ page }) => {
     await page.goto('/insights/inventory/staleness-and-deletion', {
-      timeout: 10000,
+      timeout: 100000,
     });
+
+    const emptyState = page.locator('.pf-v6-c-empty-state');
+    await expect(emptyState).toBeVisible({ timeout: 100000 });
 
     const heading = page.getByRole('heading', { level: 5 }).first();
     await expect(heading).toContainText(NO_ACCESS_STALENESS);
--- _playwright-tests/rbac/test_viewer_role_access.test.ts
+++ _playwright-tests/rbac/test_viewer_role_access.test.ts
@@ -1,9 +1,85 @@
 import { test } from '../helpers/fixtures';
+import { expect } from '@playwright/test';
+import {
+  navigateToInventorySystemsFunc,
+  navigateToStalenessPageFunc,
+  navigateToWorkspacesFunc,
+} from '../helpers/navHelpers';
 
 test.use({ storageState: '.auth/viewer_user.json' });
 
-test.describe('RBAC @rbac', () => {
-  test('viewer role', async ({ page }) => {
-    await page.goto('/insights/inventory/');
+test.describe('Viewer: read permissions only @rbac', () => {
+  test('Systems page', async ({ page }) => {
+    await navigateToInventorySystemsFunc(page);
+    const nameCell = page.locator('td[data-label="Name"]');
+    await expect(nameCell.first()).toBeVisible({ timeout: 20000 });
+
+    await page.getByRole('button', { name: 'Kebab toggle' }).first().click();
+
+    const menuItems = page
+      .locator('button.inventory__action-menu-item')
+      .getByRole('menuitem');
+
+    for (const item of await menuItems.all()) {
+      await expect(item).toHaveAttribute('aria-disabled', 'true');
+    }
+  });
+
+  test('System Details page', async ({ page }) => {
+    const NAME_ID = 'centos';
+    const URL_DETAILS =
+      '/insights/inventory/b7b2f60b-d80e-47a6-9a07-110aba29e90c';
+
+    await test.step("Navigate to system's details page", async () => {
+      await page.goto(URL_DETAILS, { timeout: 100000 });
+      const heading = page.getByRole('heading', { level: 1 }).first();
+      await expect(heading).toBeVisible({ timeout: 100000 });
+      await expect(heading).toContainText(NAME_ID);
+    });
+
+    await test.step('Delete and Edit buttons should be disbaled', async () => {
+      const deleteButton = page.getByRole('button', { name: 'Delete' });
+      await expect(deleteButton).toBeDisabled();
+
+      const allEditButtons = page.getByRole('button', { name: 'Edit' });
+      await expect(allEditButtons).toHaveCount(2);
+
+      for (const button of await allEditButtons.all()) {
+        await expect(button).toBeDisabled();
+      }
+    });
+  });
+
+  test('Workspaces page', async ({ page }) => {
+    await navigateToWorkspacesFunc(page);
+
+    // Create Workpsace button is disbaled for Non admin users
+    const createButton = page.locator(
+      '[data-ouia-component-id="CreateGroupButton"]',
+    );
+    await expect(createButton).toBeDisabled();
+
+    const nameCell = page.locator('td[data-label="Name"]');
+    await expect(nameCell.first()).toBeVisible({ timeout: 20000 });
+
+    await page.getByRole('button', { name: 'Kebab toggle' }).first().click();
+    const dropdown = page.locator('[data-ouia-component-type="PF6/Dropdown"]');
+
+    await expect(dropdown).toBeVisible();
+
+    const menuItems = page
+      .locator('button.inventory__action-menu-item')
+      .getByRole('menuitem');
+
+    for (const item of await menuItems.all()) {
+      await expect(item).toHaveAttribute('aria-disabled', 'true');
+    }
+  });
+
+  test('Staleness and Deletion page', async ({ page }) => {
+    await navigateToStalenessPageFunc(page);
+
+    const editButton = page.getByRole('button', { name: 'Edit' });
+    await expect(editButton).toBeDisabled();
   });
 });
