d56688e219c4912b63a0037e4f99cf973c40c9fe
zhamilya <@computercamplove>
2026-04-15 17:17:24 +0200

    fix:  check from auth

--- _playwright-tests/auth.setup.ts
+++ _playwright-tests/auth.setup.ts
@@ -1,12 +1,14 @@
 import { expect, test as setup } from '@playwright/test';
 import {
   ensureNotInPreview,
+  enableSystemsViewAndKessel,
   logInWithUser1,
   storeStorageStateAndToken,
   throwIfMissingEnvVariables,
   closePopupsIfExist,
   closeCookieBanner,
 } from './helpers/loginHelpers';
+import { isSystemsViewEnabled } from './helpers/constants';
 
 setup.describe('Setup', async () => {
   setup.describe.configure({ retries: 3 });
@@ -17,6 +19,11 @@ setup.describe('Setup', async () => {
 
   setup('Authenticate', async ({ page }) => {
     setup.setTimeout(120_000);
+
+    if (isSystemsViewEnabled) {
+      await enableSystemsViewAndKessel(page);
+    }
+
     await closePopupsIfExist(page);
     await logInWithUser1(page);
     await closeCookieBanner(page);
