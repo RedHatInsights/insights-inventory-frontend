215ec791669b8a8beb4bf372d2128fc28db78bf7
Anastasiia Surai <68393563+Siasurai@users.noreply.github.com>
2026-04-21 13:24:40 +0200

    fix(RHINENG-14708): fix mount ApplicationTab for connection-gated tabs (#3074)

--- src/ApplicationTab.js
+++ src/ApplicationTab.js
@@ -2,6 +2,7 @@ import { useConditionalRBAC } from './Utilities/hooks/useConditionalRBAC';
 import PropTypes from 'prop-types';
 import React from 'react';
 import AccessDenied from './Utilities/AccessDenied';
+import { NotConnected } from '@redhat-cloud-services/frontend-components/NotConnected';
 import {
   AdvisorTab,
   ComplianceTab,
@@ -11,7 +12,12 @@ import {
 } from './components/SystemDetails';
 import { TAB_REQUIRED_PERMISSIONS } from './constants';
 
-const ApplicationTab = ({ appName, title, ...props }) => {
+const ApplicationTab = ({
+  appName,
+  title,
+  insightsDisconnected = false,
+  ...props
+}) => {
   const { hasAccess, isOrgAdmin } = useConditionalRBAC(
     TAB_REQUIRED_PERMISSIONS[appName],
     true, // all must be fulfilled
@@ -28,24 +34,31 @@ const ApplicationTab = ({ appName, title, ...props }) => {
 
   const Tab = tabs[appName];
 
-  return hasAccess || isOrgAdmin ? (
-    <Tab {...props} />
-  ) : (
-    <AccessDenied
-      requiredPermission={TAB_REQUIRED_PERMISSIONS[appName].join(', ')}
-      description={
-        <div>
-          Contact your organization administrator(s) for more information.
-        </div>
-      }
-      title={`You do not have access to ${title}`}
-    />
-  );
+  if (!hasAccess && !isOrgAdmin) {
+    return (
+      <AccessDenied
+        requiredPermission={TAB_REQUIRED_PERMISSIONS[appName].join(', ')}
+        description={
+          <div>
+            Contact your organization administrator(s) for more information.
+          </div>
+        }
+        title={`You do not have access to ${title}`}
+      />
+    );
+  }
+
+  if (insightsDisconnected) {
+    return <NotConnected />;
+  }
+
+  return <Tab {...props} />;
 };
 
 ApplicationTab.propTypes = {
   title: PropTypes.string.isRequired,
   appName: PropTypes.string.isRequired,
+  insightsDisconnected: PropTypes.bool,
 };
 
 export default ApplicationTab;
