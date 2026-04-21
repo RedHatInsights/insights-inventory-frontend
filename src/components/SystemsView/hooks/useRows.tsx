a78ed744864d1b949567ba61f8614ee51407761c
Rastislav Hepner <rhepner@redhat.com>
2026-04-21 18:51:08 +0200

    feat: extend dataView row with system metadate

--- src/components/SystemsView/hooks/useRows.tsx
+++ src/components/SystemsView/hooks/useRows.tsx
@@ -5,13 +5,18 @@ import { RenderableColumn } from './useColumns';
 import type { SystemWithPermissions } from '../../../Utilities/hooks/useHostIdsWithKessel';
 import type { System } from './useSystemsQuery';
 
+/** DataViewTrObject Extension, `meta` points to associated system objects. */
+export type SystemsViewTableRow = DataViewTrObject & {
+  meta: System | SystemWithPermissions;
+};
+
 interface UseRowsParams {
   data?: (System | SystemWithPermissions)[];
   renderableColumns: RenderableColumn[];
 }
 
 interface UseRowsReturnValue {
-  rows: DataViewTrObject[];
+  rows: SystemsViewTableRow[];
 }
 
 export const useRows = ({
@@ -20,13 +25,14 @@ export const useRows = ({
 }: UseRowsParams): UseRowsReturnValue => {
   const mapSystemToRow = (
     system: System | SystemWithPermissions,
-  ): DataViewTrObject => {
+  ): SystemsViewTableRow => {
     const selectableColumnCells = renderableColumns
       .filter((col) => col.isShown)
       .map((col) => col.renderCell(system));
 
     return {
       id: system.id,
+      meta: system,
       row: [
         ...selectableColumnCells,
         {
