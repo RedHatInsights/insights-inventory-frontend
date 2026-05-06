9f2090d77299b32dbdfb81d826b2ca5f849212f3
Rastislav Hepner <rhepner@redhat.com>
2026-05-06 13:17:22 +0200

    feat: aggregate all columns

--- src/components/SystemsView/columns/inventory/columnDefinitions.tsx
+++ src/components/SystemsView/columns/inventory/columnDefinitions.tsx
@@ -9,55 +9,65 @@ import Tags from './cells/Tags';
 import { LastSeenColumnHeader } from '../../../../Utilities/LastSeenColumnHeader';
 import { System } from '../../hooks/useSystemsQuery';
 
-export const INITIAL_COLUMNS: RenderableColumn[] = [
-  {
-    title: 'Name',
-    key: 'name',
-    isShownByDefault: true,
-    isShown: true,
-    isUntoggleable: true,
-    sortBy: ApiOrderByEnum.DisplayName,
-    renderCell: (system: System) => (
-      <DisplayName key={`name-${system.id}`} system={system} />
-    ),
-  },
-  {
-    title: 'Workspace',
-    key: 'workspace',
-    isShownByDefault: true,
-    isShown: true,
-    sortBy: ApiOrderByEnum.GroupName,
-    renderCell: (system: System) => (
-      <Workspace key={`workspace-${system.id}`} system={system} />
-    ),
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    isShownByDefault: true,
-    isShown: true,
-    renderCell: (system: System) => (
-      <Tags key={`tags-${system.id}`} system={system} />
-    ),
-  },
-  {
-    title: 'OS',
-    key: 'os',
-    isShownByDefault: true,
-    isShown: true,
-    sortBy: ApiOrderByEnum.OperatingSystem,
-    renderCell: (system: System) => (
-      <OperatingSystem key={`os-${system.id}`} system={system} />
-    ),
-  },
-  {
-    title: <LastSeenColumnHeader />,
-    key: 'last_seen',
-    isShownByDefault: true,
-    isShown: true,
-    sortBy: ApiOrderByEnum.LastCheckIn,
-    renderCell: (system: System) => (
-      <LastSeen key={`lastseen-${system.id}`} system={system} />
-    ),
-  },
+const nameColumn: RenderableColumn = {
+  title: 'Name',
+  key: 'name',
+  isShownByDefault: true,
+  isShown: true,
+  isUntoggleable: true,
+  sortBy: ApiOrderByEnum.DisplayName,
+  renderCell: (system: System) => (
+    <DisplayName key={`name-${system.id}`} system={system} />
+  ),
+};
+
+const workspaceColumn: RenderableColumn = {
+  title: 'Workspace',
+  key: 'workspace',
+  isShownByDefault: true,
+  isShown: true,
+  sortBy: ApiOrderByEnum.GroupName,
+  renderCell: (system: System) => (
+    <Workspace key={`workspace-${system.id}`} system={system} />
+  ),
+};
+
+const tagsColumn: RenderableColumn = {
+  title: 'Tags',
+  key: 'tags',
+  isShownByDefault: true,
+  isShown: true,
+  renderCell: (system: System) => (
+    <Tags key={`tags-${system.id}`} system={system} />
+  ),
+};
+
+const operatingSystemColumn: RenderableColumn = {
+  title: 'OS',
+  key: 'os',
+  isShownByDefault: true,
+  isShown: true,
+  sortBy: ApiOrderByEnum.OperatingSystem,
+  renderCell: (system: System) => (
+    <OperatingSystem key={`os-${system.id}`} system={system} />
+  ),
+};
+
+const lastSeenColumn: RenderableColumn = {
+  title: <LastSeenColumnHeader />,
+  key: 'last_seen',
+  isShownByDefault: true,
+  isShown: true,
+  sortBy: ApiOrderByEnum.LastCheckIn,
+  renderCell: (system: System) => (
+    <LastSeen key={`lastseen-${system.id}`} system={system} />
+  ),
+};
+
+export default [
+  nameColumn,
+  workspaceColumn,
+  tagsColumn,
+  operatingSystemColumn,
+  lastSeenColumn,
 ];
