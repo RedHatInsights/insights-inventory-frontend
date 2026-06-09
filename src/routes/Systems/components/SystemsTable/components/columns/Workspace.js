1231dcf21c454045e8f2bc48a0042a8350a33890
Zhamilya Abikenova <zabikeno@redhat.com>
2026-06-09 08:38:16 -0400

    refactor(workspace): remove 'No workspace' placeholder (#3244)
    * refactor(workspace): remove 'No workspace' placeholder
    
    * refactor(workspace): apply suggestion
    
    * refactor(workspace): fix test

--- src/routes/Systems/components/SystemsTable/components/columns/Workspace.js
+++ src/routes/Systems/components/SystemsTable/components/columns/Workspace.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import isEmpty from 'lodash/isEmpty';
 import PropTypes from 'prop-types';
 
-const Workspace = ({ groups }) =>
-  isEmpty(groups) ? (
-    <div className="pf-v5-u-disabled-color-200">No workspace</div>
-  ) : (
-    groups[0].name
-  );
+const Workspace = ({ groups }) => groups?.[0]?.name;
 
 Workspace.propTypes = {
   groups: PropTypes.arrayOf(
