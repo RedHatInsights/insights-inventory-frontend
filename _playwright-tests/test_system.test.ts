78a65a9dd7aaa580469ff89ddf9c44512f98c2ea
Zhamilya Abikenova <zabikeno@redhat.com>
2026-06-25 16:01:44 +0200

    test: in serial mode systems tests

--- _playwright-tests/test_system.test.ts
+++ _playwright-tests/test_system.test.ts
@@ -6,6 +6,7 @@ import { searchByName, waitForTableKebabReady } from './helpers/filterHelpers';
 import { isSystemsViewEnabled } from './helpers/constants';
 
 test.describe('System CRUD', { tag: ['@systems-table'] }, () => {
+  test.describe.configure({ mode: 'serial' });
   test('User should be able to edit and delete a system from Systems page', async ({
     page,
   }) => {
