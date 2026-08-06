5a1e6be54e029e38436dbef47df0ebda05da93e0
Rastislav Hepner <rhepner@redhat.com>
2026-08-06 14:40:06 +0200

    refactor: remove PageSection nesting

--- src/components/InventoryViews/ImagesView/ImagesView.tsx
+++ src/components/InventoryViews/ImagesView/ImagesView.tsx
@@ -8,7 +8,7 @@ import { useRows } from './hooks/useRows';
 import { NO_HEADER } from '../constants';
 import NoEntitiesFound from '../../InventoryTable/NoEntitiesFound';
 import { ErrorState } from '@patternfly/react-component-groups';
-import { Bullseye, Spinner } from '@patternfly/react-core';
+import { Bullseye, PageSection, Spinner } from '@patternfly/react-core';
 import { useImageQueries } from './hooks/useImageQueries';
 import './ImagesView.scss';
 
@@ -46,32 +46,34 @@ export const ImagesView = () => {
   });
 
   return (
-    <DataView activeState={activeState}>
-      <DataViewTable
-        ouiaId="images-view-table"
-        columns={columns}
-        rows={rows}
-        headStates={{
-          loading: NO_HEADER,
-          empty: NO_HEADER,
-          error: NO_HEADER,
-        }}
-        bodyStates={{
-          loading: (
-            <Bullseye>
-              <Spinner />
-            </Bullseye>
-          ),
-          empty: <NoEntitiesFound entities="images" />,
-          error: (
-            <ErrorState
-              ouiaId="error-state"
-              titleText="Unable to load data"
-              bodyText="There was an error retrieving data. Check your connection and reload the page."
-            />
-          ),
-        }}
-      />
-    </DataView>
+    <PageSection hasBodyWrapper={false}>
+      <DataView activeState={activeState}>
+        <DataViewTable
+          ouiaId="images-view-table"
+          columns={columns}
+          rows={rows}
+          headStates={{
+            loading: NO_HEADER,
+            empty: NO_HEADER,
+            error: NO_HEADER,
+          }}
+          bodyStates={{
+            loading: (
+              <Bullseye>
+                <Spinner />
+              </Bullseye>
+            ),
+            empty: <NoEntitiesFound entities="images" />,
+            error: (
+              <ErrorState
+                ouiaId="error-state"
+                titleText="Unable to load data"
+                bodyText="There was an error retrieving data. Check your connection and reload the page."
+              />
+            ),
+          }}
+        />
+      </DataView>
+    </PageSection>
   );
 };
